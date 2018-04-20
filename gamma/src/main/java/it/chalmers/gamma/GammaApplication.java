0013d0f5e2db932f3ab00303aa86fef7871aeafa
portals <theodor.ang.98@gmail.com>
2018-04-20 19:51:29 +0200

    Created project

---
+++ gamma/src/main/java/it/chalmers/gamma/GammaApplication.java
@@ -0,0 +1,12 @@
+package it.chalmers.gamma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GammaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GammaApplication.class, args);
+	}
+}
