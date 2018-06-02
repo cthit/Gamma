65d70fee5ee3eff2619437c10bd4863465c075d6
Gustav Engsmyre <gustav.engsmyre@gmail.com>
2018-06-02 18:10:25 +0200

    #23, #22 Add some tests to the whitelist table.
    Add some safety when dealing with the whitelist table.

--- backend/src/main/java/it/chalmers/gamma/service/WhitelistService.java
+++ backend/src/main/java/it/chalmers/gamma/service/WhitelistService.java
@@ -2,6 +2,7 @@ package it.chalmers.gamma.service;
 
 import it.chalmers.gamma.db.entity.Whitelist;
 import it.chalmers.gamma.db.repository.WhitelistRepository;
+import it.chalmers.gamma.exceptions.CIDAlreadyWhitelistedException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,9 +18,21 @@ public class WhitelistService {
     public List<Whitelist> findAll(){
         return whitelistRepository.findAll();
     }
-    public Whitelist addWhiteListedCID(String cid){
+    public Whitelist addWhiteListedCID(String cid) throws CIDAlreadyWhitelistedException {
+        if(isCIDWhiteListed(cid)){
+            throw new CIDAlreadyWhitelistedException();
+        }
         Whitelist whitelistedCID = new Whitelist(cid);
-
+        System.out.println("white listed " + cid);
         return whitelistRepository.save(whitelistedCID);
+    }
+    public void removeWhiteListedCID() throws Exception{
+
+    }
+    public boolean isCIDWhiteListed(String cid){
+        if(whitelistRepository.findByCid(cid) == null){
+            return false;
+        }
+        return true;
     }
 }
---
+++ backend/src/test/java/it/chalmers/gamma/AccountTests.java
@@ -0,0 +1,56 @@
+package it.chalmers.gamma;
+
+import it.chalmers.gamma.exceptions.CIDAlreadyWhitelistedException;
+import it.chalmers.gamma.service.WhitelistService;
+import javafx.application.Application;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.validation.constraints.AssertFalse;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AccountTests {
+
+    @Autowired
+    WhitelistService whitelistService;
+
+    /**
+     * Tests if adding a CID to the whitelist works, and if seeing if a non-whitelisted CID returns correct result.
+     */
+    @Test
+    public void testWhiteList(){
+        try {
+            whitelistService.addWhiteListedCID("engsmyre");
+            whitelistService.addWhiteListedCID("svenel");
+        } catch (CIDAlreadyWhitelistedException e) {
+            e.printStackTrace();
+        }
+        Assert.assertTrue(whitelistService.isCIDWhiteListed("engsmyre"));
+        Assert.assertFalse(whitelistService.isCIDWhiteListed("leif"));
+    }
+
+    /**
+     * Tests if a CID can be added to the whitelisted database more than once.
+     */
+    @Test
+    public void testAddWhitelistCIDMultipleTimes(){
+        try {
+            whitelistService.addWhiteListedCID("engsmyre");
+        } catch (CIDAlreadyWhitelistedException e) {
+            e.printStackTrace();
+        }
+        try {
+            whitelistService.addWhiteListedCID("engsmyre");
+            assert(false);
+        } catch (CIDAlreadyWhitelistedException e) {
+            e.printStackTrace();
+            assert(true);
+        }
+    }
+}
