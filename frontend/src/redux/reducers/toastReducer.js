3550f4535231a5771456fc90c35237feb73c8a1b
portals <theodor.ang.98@gmail.com>
2018-06-20 17:37:25 +0200

    Removed debug print

--- frontend/src/redux/reducers/toastReducer.js
+++ frontend/src/redux/reducers/toastReducer.js
@@ -1,7 +1,6 @@
 import { TOAST_OPEN } from "../actions/toastActions";
 
 export function toast(state = {}, action) {
-  console.log(action);
   switch (action.type) {
     case TOAST_OPEN:
       return action.payload;
