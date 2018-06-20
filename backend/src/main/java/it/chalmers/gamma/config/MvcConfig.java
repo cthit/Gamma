6af4a19dcf81dcf0b64150b1d5312c9cbe2642c7
portals <theodor.ang.98@gmail.com>
2018-06-20 13:47:34 +0200

    Added cors mapping

--- backend/src/main/java/it/chalmers/gamma/config/MvcConfig.java
+++ backend/src/main/java/it/chalmers/gamma/config/MvcConfig.java
@@ -9,4 +9,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") //http://localhost:3000
+                .allowedMethods("GET", "POST", "UPDATE", "PUT", "DELETE");
+    }
 }
