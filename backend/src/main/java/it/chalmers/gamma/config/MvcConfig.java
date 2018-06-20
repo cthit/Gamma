0f0d1a4ec0afbae2bd29dc39683a3d546bafca6f
portals <theodor.ang.98@gmail.com>
2018-06-20 08:39:25 +0200

    Created MvcConfig class

---
+++ backend/src/main/java/it/chalmers/gamma/config/MvcConfig.java
@@ -0,0 +1,12 @@
+package it.chalmers.gamma.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+
+}
