24f4120542be299cb4a288b98cb1a46e8eace69b
portals <theodor.ang.98@gmail.com>
2018-06-21 20:38:14 +0200

    Creatd GammaRadioButtonGroup

---
+++ frontend/src/common/gui/gamma-radio-button-group/index.js
@@ -0,0 +1,76 @@
+import React from "react";
+
+import PropTypes from "prop-types";
+
+import {
+  Radio,
+  FormControlLabel,
+  RadioGroup,
+  FormHelperText
+} from "@material-ui/core";
+
+import { UpperLabel, StyledFormControl } from "./styles";
+import generateId from "../../utils/generateId";
+
+export class GammaRadioButtonGroup extends React.Component {
+  state = {
+    value: this.props.startValue != null ? this.props.startValue : ""
+  };
+
+  _handleChange = event => {
+    this.setState({ value: event.target.value });
+  };
+
+  render() {
+    return (
+      <StyledFormControl component="fieldset">
+        <UpperLabel component="legend">{this.props.upperLabel}</UpperLabel>
+        <RadioGroup
+          value={this.state.value}
+          onChange={event => {
+            this.props.onChange(event.target.value);
+            this._handleChange(event);
+          }}
+        >
+          {Object.keys(this.props.radioButtonMap)
+            .reverse()
+            .map(value => {
+              const {
+                label,
+                disabled,
+                primary,
+                secondary
+              } = this.props.radioButtonMap[value];
+              return (
+                <FormControlLabel
+                  key={value}
+                  value={value}
+                  label={label}
+                  disabled={disabled}
+                  control={
+                    <Radio
+                      color={
+                        primary
+                          ? "primary"
+                          : secondary
+                            ? "secondary"
+                            : "default"
+                      }
+                    />
+                  }
+                />
+              );
+            })}
+        </RadioGroup>
+        <FormHelperText>{this.props.lowerLabel}</FormHelperText>
+      </StyledFormControl>
+    );
+  }
+}
+
+GammaRadioButtonGroup.propTypes = {
+  radioButtonMap: PropTypes.object.isRequired,
+  onChange: PropTypes.func.isRequired,
+  upperLabel: PropTypes.string,
+  lowerLabel: PropTypes.string
+};
---
+++ frontend/src/common/gui/gamma-radio-button-group/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+import { FormControl, FormLabel } from "@material-ui/core";
+
+export const UpperLabel = styled(FormLabel)`
+  /*material-ui is somewhat buggy and change the colors randomly*/
+  color: rgba(0, 0, 0, 0.54) !important;
+`;
+
+export const StyledFormControl = styled(FormControl)``;
