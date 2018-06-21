7b3d6d49d505ccdd76118e18d85d9ba2cc061186
Gustav Engsmyre <gustav.engsmyre@gmail.com>
2018-06-21 22:09:12 +0200

    add saving the account using the activation_code

--- backend/src/main/java/it/chalmers/gamma/service/ActivationCodeService.java
+++ backend/src/main/java/it/chalmers/gamma/service/ActivationCodeService.java
@@ -3,11 +3,8 @@ package it.chalmers.gamma.service;
 import it.chalmers.gamma.db.entity.ActivationCode;
 import it.chalmers.gamma.db.entity.Whitelist;
 import it.chalmers.gamma.db.repository.ActivationCodeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.mail.MessagingException;
-import java.util.ArrayList;
 import java.util.Random;
 import java.util.UUID;
 @Service
@@ -15,16 +12,13 @@ public class ActivationCodeService {
 
     private final ActivationCodeRepository activationCodeRepository;
 
-    @Autowired
-    MailSenderService mailService;
-
     private String[] words = {"ITSMURFEN", "DIGIT<3DIDIT", "SOCKERARGOTT", "HUBBEN2.0"};        // Add some random words in here.
 
     public ActivationCodeService(ActivationCodeRepository activationCodeRepository){
         this.activationCodeRepository = activationCodeRepository;
     }
 
-    private String generateActivationCode(){
+    public String generateActivationCode(){
         Random rand = new Random();
         StringBuilder word = new StringBuilder(words[rand.nextInt(words.length-1)]);
         char[] code = new char[word.length()];
@@ -36,14 +30,18 @@ public class ActivationCodeService {
         return String.valueOf(code);
     }
 
-    public String generateAndSaveActivationCode(Whitelist cid){
-        String code = generateActivationCode();
+    public ActivationCode saveActivationCode(Whitelist cid, String code){
         ActivationCode activationCode = new ActivationCode(cid);
         activationCode.setId(UUID.randomUUID());
         activationCode.setCode(code);
         activationCodeRepository.save(activationCode);
-        sendEmail(activationCode);
-        return code;
+        return activationCode;
+    }
+
+    public boolean codeMatches(String code, Whitelist user){
+        System.out.println("user is: " + activationCodeRepository.findByCid(user));
+
+        return true;
     }
 
     // TODO Delete entry after 1 hour or once code has been used. This does not work.
@@ -51,15 +49,5 @@ public class ActivationCodeService {
         activationCodeRepository.delete(activationCodeRepository.findByCid(cid));
     }
 
-    private void sendEmail(ActivationCode activationCode){
-        String code = activationCode.getCode();
-        String to = activationCode.getCid() + "@student.chalmers.se";
-        String message = "Your code to Gamma is: " + code;
-        try {
-            mailService.sendMessage(to, "login code", message);
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
-    }
 
 }
--- backend/src/main/java/it/chalmers/gamma/service/ITUserService.java
+++ backend/src/main/java/it/chalmers/gamma/service/ITUserService.java
@@ -61,7 +61,7 @@ public class ITUserService implements UserDetailsService {
         itUser.setFirstName(user.getFirstName());
         itUser.setLastName(user.getLastName());
         String currentTime = String.valueOf(System.currentTimeMillis());
-        itUser.setCid(user.getNick()); //Todo get cid from activation code
+        itUser.setCid(user.getCid().getCid()); //Todo get cid from activation code
         itUser.setAcceptanceYear(Year.of(2017));
         itUser.setUserAgreement(user.isUserAgreement());
         itUser.setGdpr(false);
