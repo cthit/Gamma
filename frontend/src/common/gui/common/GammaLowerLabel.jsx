f057fe7da2e9d831bba6b77561df213c760a76de
portals <theodor.ang.98@gmail.com>
2018-06-24 10:16:00 +0200

    Added proptypes to GammaLowerLabel

--- frontend/src/common/gui/common/GammaLowerLabel.jsx
+++ frontend/src/common/gui/common/GammaLowerLabel.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+
+import PropTypes from "prop-types";
+
 import { FormHelperText } from "@material-ui/core";
 
 export const GammaLowerLabel = ({ text }) => (
   <FormHelperText>{text}</FormHelperText>
 );
+
+GammaLowerLabel.propTypes = {
+  text: PropTypes.string.isRequired
+};
