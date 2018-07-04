a4bf72b511b3febcb09a09b9a8239aa4bfa7806d
portals <theodor.ang.98@gmail.com>
2018-07-04 15:52:21 +0200

    Made InputDataAndCode stateless #54

--- frontend/src/use-cases/create-account/views/input-data-and-code/InputDataAndCode.view.jsx
+++ frontend/src/use-cases/create-account/views/input-data-and-code/InputDataAndCode.view.jsx
@@ -26,189 +26,164 @@ import {
   GammaCardTitle
 } from "../../../../common-ui/design";
 
-class InputDataAndCode extends React.Component {
-  render() {
-    //Functions
-    const { sendDataAndCode } = this.props;
+const InputDataAndCode = ({ sendDataAndCode, text }) => (
+  <Center>
+    <GammaForm
+      onSubmit={(values, actions) => {
+        const cid = values.cid;
+        delete values.cid;
+        const user = {
+          whitelist: {
+            cid: cid
+          },
+          ...values
+        };
+        sendDataAndCode(user);
+        actions.resetForm();
+      }}
+      initialValues={{
+        cid: "",
+        code: "",
+        nick: "",
+        firstName: "",
+        lastName: "",
+        attendanceYear: "2018",
+        password: "",
+        passwordConfirmation: "",
+        userAgreement: false
+      }}
+      validationSchema={yup.object().shape({
+        cid: yup.string().required(text.FieldRequired),
+        code: yup.string().required(text.FieldRequired),
+        nick: yup.string().required(text.FieldRequired),
+        firstName: yup.string().required(text.FieldRequired),
+        lastName: yup.string().required(text.FieldRequired),
+        attendanceYear: yup
+          .number()
+          .min(2001)
+          .max(_getCurrentYear())
+          .required(text.FieldRequired),
+        password: yup.string().required(text.FieldRequired),
+        passwordConfirmation: yup
+          .string()
+          .oneOf([yup.ref("password")], text.PasswordsDoNotMatch)
+          .required(text.FieldRequired),
+        userAgreement: yup
+          .boolean()
+          .oneOf([true])
+          .required(text.FieldRequired)
+      })}
+      render={props => (
+        <GammaCard minWidth="300px" maxWidth="600px" hasSubTitle>
+          <GammaCardTitle text={text.CompleteCreation} />
+          <GammaCardSubTitle text={text.CompleteCreationDescription} />
+          <GammaCardBody>
+            <Center>
+              <GammaFormField
+                name="cid"
+                component={ConfirmCidInput}
+                componentProps={{
+                  upperLabel: text.YourCid
+                }}
+              />
+              <Spacing />
+              <GammaFormField
+                name="code"
+                component={ConfirmationCodeInput}
+                componentProps={{
+                  upperLabel: text.CodeFromYourStudentEmail
+                }}
+              />
+              <Spacing />
+              <GammaFormField
+                name="nick"
+                component={NickInput}
+                componentProps={{
+                  upperLabel: text.Nick
+                }}
+              />
+              <Spacing />
+              <GammaFormField
+                name="password"
+                component={PasswordInput}
+                componentProps={{
+                  upperLabel: text.Password,
+                  password: true
+                }}
+              />
+              <Spacing />
+              <GammaFormField
+                name="passwordConfirmation"
+                component={PasswordConfirmationInput}
+                componentProps={{
+                  upperLabel: text.ConfirmPassword,
+                  password: true
+                }}
+              />
+              <Spacing />
+              <GammaFormField
+                name="firstName"
+                component={FirstnameInput}
+                componentProps={{
+                  upperLabel: text.FirstName
+                }}
+              />
+              <Spacing />
+              <GammaFormField
+                name="lastName"
+                component={LastnameInput}
+                componentProps={{
+                  upperLabel: text.LastName
+                }}
+              />
+              <Spacing />
+              <GammaFormField
+                name="attendanceYear"
+                component={AttendanceYearInput}
+                componentProps={{
+                  valueToTextMap: _generateAttendanceYears(),
+                  upperLabel: text.WhichYearDidYouStart,
+                  reverse: true
+                }}
+              />
+              <Spacing />
+              <GammaFormField
+                name="userAgreement"
+                component={AcceptUserAgreementInput}
+                componentProps={{
+                  label: text.AcceptUserAgreement,
+                  primary: true
+                }}
+              />
+            </Center>
+          </GammaCardBody>
+          <GammaCardButtons leftRight reverseDirection>
+            <CreateAccountButton
+              submit
+              text={text.CreateAccount}
+              primary
+              raised
+            />
+            <Spacing />
+          </GammaCardButtons>
+        </GammaCard>
+      )}
+    />
+  </Center>
+);
 
-    //Texts
-    const {
-      CompleteCreation,
-      CompleteCreationDescription,
-      YourCid,
-      CodeFromYourStudentEmail,
-      Nick,
-      Password,
-      ConfirmPassword,
-      FirstName,
-      LastName,
-      WhichYearDidYouStart,
-      AcceptUserAgreement,
-      CreateAccount,
-
-      PasswordsDoNotMatch,
-      FieldRequired
-    } = this.props.text;
-
-    return (
-      <Center>
-        <GammaForm
-          onSubmit={(values, actions) => {
-            const cid = values.cid;
-            delete values.cid;
-            const user = {
-              whitelist: {
-                cid: cid
-              },
-              ...values
-            };
-            sendDataAndCode(user);
-            actions.resetForm();
-          }}
-          initialValues={{
-            cid: "",
-            code: "",
-            nick: "",
-            firstName: "",
-            lastName: "",
-            attendanceYear: "2018",
-            password: "",
-            passwordConfirmation: "",
-            userAgreement: false
-          }}
-          validationSchema={yup.object().shape({
-            cid: yup.string().required(FieldRequired),
-            code: yup.string().required(FieldRequired),
-            nick: yup.string().required(FieldRequired),
-            firstName: yup.string().required(FieldRequired),
-            lastName: yup.string().required(FieldRequired),
-            attendanceYear: yup
-              .number()
-              .min(2001)
-              .max(this._getCurrentYear())
-              .required(FieldRequired),
-            password: yup.string().required(FieldRequired),
-            passwordConfirmation: yup
-              .string()
-              .oneOf([yup.ref("password")], PasswordsDoNotMatch)
-              .required(FieldRequired),
-            userAgreement: yup
-              .boolean()
-              .oneOf([true])
-              .required(FieldRequired)
-          })}
-          render={props => (
-            <GammaCard minWidth="300px" maxWidth="600px" hasSubTitle>
-              <GammaCardTitle text={CompleteCreation} />
-              <GammaCardSubTitle text={CompleteCreationDescription} />
-              <GammaCardBody>
-                <Center>
-                  <GammaFormField
-                    name="cid"
-                    component={ConfirmCidInput}
-                    componentProps={{
-                      upperLabel: YourCid
-                    }}
-                  />
-                  <Spacing />
-                  <GammaFormField
-                    name="code"
-                    component={ConfirmationCodeInput}
-                    componentProps={{
-                      upperLabel: CodeFromYourStudentEmail
-                    }}
-                  />
-                  <Spacing />
-                  <GammaFormField
-                    name="nick"
-                    component={NickInput}
-                    componentProps={{
-                      upperLabel: Nick
-                    }}
-                  />
-                  <Spacing />
-                  <GammaFormField
-                    name="password"
-                    component={PasswordInput}
-                    componentProps={{
-                      upperLabel: Password,
-                      password: true
-                    }}
-                  />
-                  <Spacing />
-                  <GammaFormField
-                    name="passwordConfirmation"
-                    component={PasswordConfirmationInput}
-                    componentProps={{
-                      upperLabel: ConfirmPassword,
-                      password: true
-                    }}
-                  />
-                  <Spacing />
-                  <GammaFormField
-                    name="firstName"
-                    component={FirstnameInput}
-                    componentProps={{
-                      upperLabel: FirstName
-                    }}
-                  />
-                  <Spacing />
-                  <GammaFormField
-                    name="lastName"
-                    component={LastnameInput}
-                    componentProps={{
-                      upperLabel: LastName
-                    }}
-                  />
-                  <Spacing />
-                  <GammaFormField
-                    name="attendanceYear"
-                    component={AttendanceYearInput}
-                    componentProps={{
-                      valueToTextMap: this._generateAttendanceYears(),
-                      upperLabel: WhichYearDidYouStart,
-                      reverse: true
-                    }}
-                  />
-                  <Spacing />
-                  <GammaFormField
-                    name="userAgreement"
-                    component={AcceptUserAgreementInput}
-                    componentProps={{
-                      label: AcceptUserAgreement,
-                      primary: true
-                    }}
-                  />
-                </Center>
-              </GammaCardBody>
-              <GammaCardButtons leftRight reverseDirection>
-                <CreateAccountButton
-                  submit
-                  text={CreateAccount}
-                  primary
-                  raised
-                />
-                <Spacing />
-              </GammaCardButtons>
-            </GammaCard>
-          )}
-        />
-      </Center>
-    );
-  }
-
-  _getCurrentYear() {
-    return new Date().getFullYear() + "";
-  }
-
-  _generateAttendanceYears() {
-    const output = {};
-    const startYear = 2001;
-    const currentYear = this._getCurrentYear();
-    for (var i = currentYear; i >= startYear; i--) {
-      output[i] = i + "";
-    }
-    return output;
-  }
+function _getCurrentYear() {
+  return new Date().getFullYear() + "";
 }
+
+function _generateAttendanceYears() {
+  const output = {};
+  const startYear = 2001;
+  const currentYear = _getCurrentYear();
+  for (var i = currentYear; i >= startYear; i--) {
+    output[i] = i + "";
+  }
+  return output;
+}
+
 export default InputDataAndCode;
