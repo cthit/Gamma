35a07c8d626b0a4178b948d82a00d9d4d06ad2a8
Gustav Engsmyre <gustav.engsmyre@gmail.com>
2018-07-18 15:41:25 +0200

    fix so wrong jwt code does not produce 500 error, but 403 Access denied

--- backend/src/main/java/it/chalmers/gamma/jwt/JwtTokenFilter.java
+++ backend/src/main/java/it/chalmers/gamma/jwt/JwtTokenFilter.java
@@ -1,5 +1,8 @@
 package it.chalmers.gamma.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.MalformedJwtException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
@@ -20,10 +23,14 @@ public class JwtTokenFilter extends GenericFilterBean {
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException{
         String encodedToken = jwtTokenProvider.resolveToken((HttpServletRequest) request);
         if(encodedToken != null) {
-            String token = jwtTokenProvider.decodeToken(encodedToken).getBody().getSubject();
+             Jws<Claims> claim = jwtTokenProvider.decodeToken(encodedToken);
+             String token = null;
+             if(claim != null){
+                 token = claim.getBody().getSubject();
+             }
             if (token != null && jwtTokenProvider.validateToken(encodedToken)) {
                 Authentication auth = jwtTokenProvider.getAuthentication(token);
                 SecurityContextHolder.getContext().setAuthentication(auth);
