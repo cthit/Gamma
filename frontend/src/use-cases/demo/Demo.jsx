f5163baf39a5ea073620572f658e0af5395a0547
portals <theodor.ang.98@gmail.com>
2018-07-20 15:00:03 +0200

    Removed old demo code

--- frontend/src/use-cases/demo/Demo.jsx
+++ frontend/src/use-cases/demo/Demo.jsx
@@ -1,30 +1,6 @@
 import React from "react";
 import { Add } from "@material-ui/icons";
 
-import {
-  ButtonsContainer,
-  CheckboxesContainer,
-  IconButtonsContainer,
-  MenusContainer,
-  RadioButtonsContainer,
-  SteppersContainer,
-  SwitchesContainer,
-  TextFieldsContainer,
-  SelectsContainer
-} from "./Demo.styles";
-
-import LoginForm from "./elements/login-form";
-
-import GammaButton from "../../common/elements/gamma-button";
-import GammaCheckbox from "../../common/elements/gamma-checkbox";
-import GammaIconButton from "../../common/elements/gamma-icon-button";
-import GammaMenu from "../../common/views/gamma-menu";
-import GammaRadioButtonGroup from "../../common/elements/gamma-radio-button-group";
-import GammaStepper from "../../common/elements/gamma-stepper";
-import GammaSwitch from "../../common/elements/gamma-switch";
-import GammaTextField from "../../common/elements/gamma-text-field";
-import GammaSelect from "../../common/elements/gamma-select";
-
 import { Heading, Title, Subtitle } from "../../common-ui/text";
 
 class Demo extends React.Component {
@@ -33,14 +9,7 @@ class Demo extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <Heading text="Hej" />
-        <Title text="Det här är en title" />
-        <Subtitle text="Det här är en subtitle" />
-        <LoginForm />
-      </div>
-    );
+    return <div />;
   }
 }
 
