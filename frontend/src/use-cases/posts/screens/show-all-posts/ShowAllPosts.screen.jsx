33b8b8ae58a2786675392f2ca1442e8699cbf0ac
portals <theodor.ang.98@gmail.com>
2018-07-25 10:49:35 +0200

    Added GammaFABButtons for editing and adding of posts

--- frontend/src/use-cases/posts/screens/show-all-posts/ShowAllPosts.screen.jsx
+++ frontend/src/use-cases/posts/screens/show-all-posts/ShowAllPosts.screen.jsx
@@ -1,24 +1,33 @@
 import React from "react";
 import GammaTable from "../../../../common/views/gamma-table";
+import { Fill } from "../../../../common-ui/layout";
+import GammaFABButton from "../../../../common/elements/gamma-fab-button";
+import { Add } from "@material-ui/icons";
+import { GammaLink } from "../../../../common-ui/design";
 
 const ShowAllPosts = ({ posts, text }) => (
-  <GammaTable
-    idProp="id"
-    startOrderBy="sv"
-    columnsOrder={["id", "sv", "en"]}
-    headerTexts={{
-      id: "Id",
-      sv: "Svenska",
-      en: "Engelska",
-      __link: "Detaljer"
-    }}
-    data={posts.map(post => {
-      return {
-        ...post,
-        __link: "/posts/" + post.id
-      };
-    })}
-  />
+  <Fill>
+    <GammaTable
+      idProp="id"
+      startOrderBy="sv"
+      columnsOrder={["id", "sv", "en"]}
+      headerTexts={{
+        id: "Id",
+        sv: "Svenska",
+        en: "Engelska",
+        __link: "Detaljer"
+      }}
+      data={posts.map(post => {
+        return {
+          ...post,
+          __link: "/posts/" + post.id
+        };
+      })}
+    />
+    <GammaLink to="/posts/add">
+      <GammaFABButton component={Add} secondary />
+    </GammaLink>
+  </Fill>
 );
 
 export default ShowAllPosts;
--- frontend/src/use-cases/posts/screens/show-post-details/ShowPostDetails.screen.jsx
+++ frontend/src/use-cases/posts/screens/show-post-details/ShowPostDetails.screen.jsx
@@ -5,41 +5,49 @@ import { Fill, Center, Spacing } from "../../../../common-ui/layout";
 import {
   GammaCard,
   GammaCardTitle,
-  GammaCardBody
+  GammaCardBody,
+  GammaLink
 } from "../../../../common-ui/design";
 import IfElseRendering from "../../../../common/declaratives/if-else-rendering";
 import { Text } from "../../../../common-ui/text";
+import GammaFABButton from "../../../../common/elements/gamma-fab-button";
+import { Edit } from "@material-ui/icons";
 
 const PostInformationOrder = ["id", "sv", "en"];
 
 const ShowPostDetails = ({ post, text }) => (
-  <Center>
-    <IfElseRendering
-      test={post != null}
-      ifRender={() => (
-        <GammaCard minWidth="300px" maxWidth="600px">
-          <GammaCardBody>
-            <HorizontalContainer>
-              <RightAlignFill>
-                {PostInformationOrder.map(prop => (
-                  <Text key={prop} bold text={text[prop] + ": "} />
-                ))}
-              </RightAlignFill>
-              <Spacing />
-              <LeftAlignFill>
-                {PostInformationOrder.map(prop => (
-                  <Text
-                    key={prop}
-                    text={post[prop] == null ? "null" : post[prop]}
-                  />
-                ))}
-              </LeftAlignFill>
-            </HorizontalContainer>
-          </GammaCardBody>
-        </GammaCard>
-      )}
-    />
-  </Center>
+  <IfElseRendering
+    test={post != null}
+    ifRender={() => (
+      <Fill>
+        <Center>
+          <GammaCard minWidth="300px" maxWidth="600px">
+            <GammaCardBody>
+              <HorizontalContainer>
+                <RightAlignFill>
+                  {PostInformationOrder.map(prop => (
+                    <Text key={prop} bold text={text[prop] + ": "} />
+                  ))}
+                </RightAlignFill>
+                <Spacing />
+                <LeftAlignFill>
+                  {PostInformationOrder.map(prop => (
+                    <Text
+                      key={prop}
+                      text={post[prop] == null ? "null" : post[prop]}
+                    />
+                  ))}
+                </LeftAlignFill>
+              </HorizontalContainer>
+            </GammaCardBody>
+          </GammaCard>
+        </Center>
+        <GammaLink to={"/posts/" + post.id + "/edit"}>
+          <GammaFABButton component={Edit} secondary />
+        </GammaLink>
+      </Fill>
+    )}
+  />
 );
 
 export default ShowPostDetails;
