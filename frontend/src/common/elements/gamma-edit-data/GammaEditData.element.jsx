bf155a86a7c6d9bea42fdde6bfb48b5f5eaa969d
portals <theodor.ang.98@gmail.com>
2018-07-27 12:56:22 +0200

    Fixes bug where initialValues wasn't ready

--- frontend/src/common/elements/gamma-edit-data/GammaEditData.element.jsx
+++ frontend/src/common/elements/gamma-edit-data/GammaEditData.element.jsx
@@ -9,7 +9,7 @@ import {
 } from "../../../common-ui/design";
 import GammaFormField from "../gamma-form-field";
 import GammaButton from "../gamma-button";
-
+import IfElseRendering from "../../declaratives/if-else-rendering";
 const GammaEditData = ({
   initialValues,
   validationSchema,
@@ -19,30 +19,35 @@ const GammaEditData = ({
   titleText,
   submitText
 }) => (
-  <GammaForm
-    validationSchema={validationSchema}
-    initialValues={initialValues}
-    onSubmit={onSubmit}
-    render={({ errors, touched }) => (
-      <GammaCard minWidth="300px" maxWidth="600px">
-        <GammaCardTitle text={titleText} />
-        <GammaCardBody>
-          {keysOrder.map(key => {
-            const keyComponentData = keysComponentData[key];
-            return (
-              <GammaFormField
-                key={key}
-                name={key}
-                component={keyComponentData.component}
-                componentProps={keyComponentData.componentProps}
-              />
-            );
-          })}
-        </GammaCardBody>
-        <GammaCardButtons reverseDirection>
-          <GammaButton submit text={submitText} raised primary />
-        </GammaCardButtons>
-      </GammaCard>
+  <IfElseRendering
+    test={initialValues != null}
+    ifRender={() => (
+      <GammaForm
+        validationSchema={validationSchema}
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        render={({ errors, touched }) => (
+          <GammaCard minWidth="300px" maxWidth="600px">
+            <GammaCardTitle text={titleText} />
+            <GammaCardBody>
+              {keysOrder.map(key => {
+                const keyComponentData = keysComponentData[key];
+                return (
+                  <GammaFormField
+                    key={key}
+                    name={key}
+                    component={keyComponentData.component}
+                    componentProps={keyComponentData.componentProps}
+                  />
+                );
+              })}
+            </GammaCardBody>
+            <GammaCardButtons reverseDirection>
+              <GammaButton submit text={submitText} raised primary />
+            </GammaCardButtons>
+          </GammaCard>
+        )}
+      />
     )}
   />
 );
