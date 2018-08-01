30784438e39af7d0c672943370cb3c08cc49d113
portals <theodor.ang.98@gmail.com>
2018-08-01 09:25:51 +0200

    Deletion of groups now is implemented on the frontend

--- frontend/src/use-cases/groups/Groups.action-creator.jsx
+++ frontend/src/use-cases/groups/Groups.action-creator.jsx
@@ -65,7 +65,11 @@ export function groupsChange(group, groupId) {
   return dispatch => {
     return new Promise((resolve, reject) => {
       axios
-        .put("http://localhost:8081/admin/groups/" + groupId)
+        .put("http://localhost:8081/admin/groups/" + groupId, group, {
+          headers: {
+            Authorization: "Bearer " + token()
+          }
+        })
         .then(response => {
           dispatch(groupsChangeSuccessfully());
           resolve(response);
@@ -78,9 +82,24 @@ export function groupsChange(group, groupId) {
   };
 }
 
-export function groupsDelete() {
+export function groupsDelete(groupId) {
   return dispatch => {
-    return new Promise((resolve, reject) => {});
+    return new Promise((resolve, reject) => {
+      axios
+        .delete("http://localhost:8081/admin/groups/" + groupId, {
+          headers: {
+            Authorization: "Bearer " + token()
+          }
+        })
+        .then(response => {
+          groupsDeleteSuccessfully();
+          resolve(response);
+        })
+        .catch(error => {
+          groupsDeleteFailed(error);
+          reject(error);
+        });
+    });
   };
 }
 
@@ -149,6 +168,23 @@ function groupsChangeSuccessfully() {
 function groupsChangeFailed(error) {
   return {
     type: GROUPS_CHANGE_FAILED,
+    error: true,
+    payload: {
+      error: error
+    }
+  };
+}
+
+function groupsDeleteSuccessfully() {
+  return {
+    type: GROUPS_DELETE_SUCCESSFULLY,
+    error: false
+  };
+}
+
+function groupsDeleteFailed(error) {
+  return {
+    type: GROUPS_DELETE_FAILED,
     error: true,
     payload: {
       error: error
