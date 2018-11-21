1103657ebabba4e1b01d08824f91d635ba4e4c17
Theodor Angergård <portalprogramming@gmail.com>
2018-11-21 17:51:34 +0100

    Adds /api/users, but it's not used yet

---
+++ frontend/src/api/users/delete.users.api.js
@@ -0,0 +1,6 @@
+import { deleteRequest } from "../utils/api";
+import { USERS_ENDPOINT } from "../utils/endpoints";
+
+export function deleteUser(userId) {
+    return deleteRequest(USERS_ENDPOINT + userId);
+}
--- frontend/src/api/utils/endpoints.js
+++ frontend/src/api/utils/endpoints.js
@@ -2,3 +2,4 @@ export const WEBSITES_ENDPOINT = "/admin/websites/";
 export const WHITELIST_ENDPOINT = "/admin/users/whitelist/";
 export const POSTS_ENDPOINT = "/admin/groups/posts/";
 export const ACTIVATION_CODES_ENDPOINT = "/activation_codes/";
+export const USERS_ENDPOINT = "/admin/users/";
