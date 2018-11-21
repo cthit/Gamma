6fda58cb703e5d20a318f5647ad2c94b1fe9716b
Theodor Angergård <portalprogramming@gmail.com>
2018-11-21 16:28:53 +0100

    Adds /api/posts

---
+++ frontend/src/api/posts/put.posts.api.js
@@ -0,0 +1,14 @@
+import { putRequest } from "../utils/api";
+import { POSTS_ENDPOINT } from "../utils/endpoints";
+
+/**
+ * {
+ *     post: {
+ *         sv: String,
+ *         en: String
+ *     }
+ * }
+ */
+export function editPost(postId, newPostId) {
+    return putRequest(POSTS_ENDPOINT + postId, newPostId);
+}
--- frontend/src/api/utils/endpoints.js
+++ frontend/src/api/utils/endpoints.js
@@ -1,2 +1,3 @@
 export const WEBSITES_ENDPOINT = "/admin/websites/";
 export const WHITELIST_ENDPOINT = "/admin/users/whitelist/";
+export const POSTS_ENDPOINT = "/admin/groups/posts/";
