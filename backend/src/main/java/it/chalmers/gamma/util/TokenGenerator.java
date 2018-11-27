b4ed3ad58f0892ea678f4e0e69163243971243d8
Theodor Angergård <portalprogramming@gmail.com>
2018-11-27 23:57:06 +0100

    Fix TokenGenerator checkstyle violations

--- backend/src/main/java/it/chalmers/gamma/util/TokenGenerator.java
+++ backend/src/main/java/it/chalmers/gamma/util/TokenGenerator.java
@@ -3,17 +3,18 @@ package it.chalmers.gamma.util;
 import java.util.Random;
 
 public class TokenGenerator {
-    private static int length = 100;
-    private static String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private static String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
-    private static String NUMBERS = "123456789";
-    private static String SPECIALS = "!@#$%&*()_+-=[]|,./?><";
-    public static String generateToken(){
-        String characters = UPPERCASE + LOWERCASE + NUMBERS + SPECIALS;
+
+    public static String generateToken() {
+        String uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String lowercasee = "abcdefghijklmnopqrstuvwxyz";
+        String numbers = "123456789";
+        String specials = "!@#$%&*()_+-=[]|,./?><";
+        String characters = uppercase + lowercasee + numbers + specials;
         Random rand = new Random();
         StringBuilder code = new StringBuilder();
-        for(int i = 0; i < length; i++){
-            code.append(characters.charAt(rand.nextInt(characters.length()-1)));
+        int length = 100;
+        for (int i = 0; i < length; i++) {
+            code.append(characters.charAt(rand.nextInt(characters.length() - 1)));
         }
         return code.toString();
     }
