0c960e5f250b0ea670c836e0b4c8adb85416cc4b
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2018-11-27 18:29:09 +0100

    Fix GroupMemberAdminController checkstyle violations

--- backend/src/main/java/it/chalmers/gamma/controller/admin/GroupMemberAdminController.java
+++ backend/src/main/java/it/chalmers/gamma/controller/admin/GroupMemberAdminController.java
@@ -5,17 +5,24 @@ import it.chalmers.gamma.db.entity.ITUser;
 import it.chalmers.gamma.db.entity.Membership;
 import it.chalmers.gamma.db.entity.Post;
 import it.chalmers.gamma.requests.AddUserGroupRequest;
-import it.chalmers.gamma.response.*;
+import it.chalmers.gamma.response.CidNotFoundResponse;
+import it.chalmers.gamma.response.GetMembershipsResponse;
+import it.chalmers.gamma.response.GroupDoesNotExistResponse;
+import it.chalmers.gamma.response.PostDoesNotExistResponse;
+import it.chalmers.gamma.response.UserAddedToGroupResponse;
 import it.chalmers.gamma.service.FKITService;
 import it.chalmers.gamma.service.ITUserService;
 import it.chalmers.gamma.service.MembershipService;
 import it.chalmers.gamma.service.PostService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/admin/groups")
@@ -25,7 +32,11 @@ public class GroupMemberAdminController {
     private FKITService fkitService;
     private MembershipService membershipService;
 
-    public GroupMemberAdminController(ITUserService itUserService, PostService postService, FKITService fkitService, MembershipService membershipService){
+    public GroupMemberAdminController(
+            ITUserService itUserService,
+            PostService postService,
+            FKITService fkitService,
+            MembershipService membershipService) {
         this.itUserService = itUserService;
         this.postService = postService;
         this.fkitService = fkitService;
@@ -33,29 +44,31 @@ public class GroupMemberAdminController {
     }
 
     @RequestMapping(value = "/{id}/members", method = RequestMethod.POST)
-    public ResponseEntity<String> addUserToGroup(@RequestBody AddUserGroupRequest request, @PathVariable("id") String id) {
-        if (!itUserService.userExists(request.getUser())) {
+    public ResponseEntity<String> addUserToGroup(
+            @RequestBody AddUserGroupRequest request,
+            @PathVariable("id") String id) {
+        if (!this.itUserService.userExists(request.getUser())) {
             throw new CidNotFoundResponse();
         }
-        if (!postService.postExists(request.getPost())) {
+        if (!this.postService.postExists(request.getPost())) {
             throw new PostDoesNotExistResponse();
         }
-        ITUser user = itUserService.loadUser(request.getUser());
-        FKITGroup fkitGroup = fkitService.getGroup(UUID.fromString(id));
-        Post post = postService.getPost(request.getPost());
-        membershipService.addUserToGroup(fkitGroup, user, post, request.getUnofficialName(), request.getYear());
+        ITUser user = this.itUserService.loadUser(request.getUser());
+        FKITGroup fkitGroup = this.fkitService.getGroup(UUID.fromString(id));
+        Post post = this.postService.getPost(request.getPost());
+        this.membershipService.addUserToGroup(fkitGroup, user, post, request.getUnofficialName(), request.getYear());
         return new UserAddedToGroupResponse();
     }
     @RequestMapping(value = "/{id}/members", method = RequestMethod.GET)
-    public ResponseEntity<List<Membership>> getUsersInGroup(@PathVariable("id") String id){
-        FKITGroup group = fkitService.getGroup(UUID.fromString(id));
-        if(group == null){
+    public ResponseEntity<List<Membership>> getUsersInGroup(@PathVariable("id") String id) {
+        FKITGroup group = this.fkitService.getGroup(UUID.fromString(id));
+        if (group == null) {
             throw new GroupDoesNotExistResponse();
         }
-        List<ITUser> members = membershipService.getUsersInGroup(group);
+        List<ITUser> members = this.membershipService.getUsersInGroup(group);
         List<Membership> groupMembers = new ArrayList<>();
-        for(ITUser member : members) {
-            groupMembers.add(membershipService.getMembershipByUserAndGroup(member, group));
+        for (ITUser member : members) {
+            groupMembers.add(this.membershipService.getMembershipByUserAndGroup(member, group));
         }
         return new GetMembershipsResponse(groupMembers);
     }
