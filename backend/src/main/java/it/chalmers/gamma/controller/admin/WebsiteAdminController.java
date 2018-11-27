7ae6620b5293ed8975b6a31d7de21365928c75e6
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2018-11-27 19:00:23 +0100

    Fix WebsiteAdminController checkstyle violations

--- backend/src/main/java/it/chalmers/gamma/controller/admin/WebsiteAdminController.java
+++ backend/src/main/java/it/chalmers/gamma/controller/admin/WebsiteAdminController.java
@@ -2,12 +2,23 @@ package it.chalmers.gamma.controller.admin;
 
 import it.chalmers.gamma.db.entity.Website;
 import it.chalmers.gamma.requests.CreateWebsiteRequest;
-import it.chalmers.gamma.response.*;
-import it.chalmers.gamma.service.*;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
+import it.chalmers.gamma.response.EditedWebsiteResponse;
+import it.chalmers.gamma.response.GetAllWebsitesResponse;
+import it.chalmers.gamma.response.GetWebsiteResponse;
+import it.chalmers.gamma.response.MissingRequiredFieldResponse;
+import it.chalmers.gamma.response.WebsiteAddedResponse;
+import it.chalmers.gamma.response.WebsiteDeletedResponse;
+import it.chalmers.gamma.response.WebsiteNotFoundResponse;
+import it.chalmers.gamma.service.GroupWebsiteService;
+import it.chalmers.gamma.service.UserWebsiteService;
+import it.chalmers.gamma.service.WebsiteService;
 import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/admin/websites")
@@ -18,7 +29,10 @@ public class WebsiteAdminController {
     private UserWebsiteService userWebsiteService;
 
 
-    public WebsiteAdminController(WebsiteService websiteService, GroupWebsiteService groupWebsiteService, UserWebsiteService userWebsiteService){
+    public WebsiteAdminController(
+            WebsiteService websiteService,
+            GroupWebsiteService groupWebsiteService,
+            UserWebsiteService userWebsiteService) {
         this.websiteService = websiteService;
         this.groupWebsiteService = groupWebsiteService;
         this.userWebsiteService = userWebsiteService;
@@ -26,40 +40,42 @@ public class WebsiteAdminController {
 
 
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<String> addWebsite(@RequestBody CreateWebsiteRequest request){
-        if(request.getName() == null){
+    public ResponseEntity<String> addWebsite(@RequestBody CreateWebsiteRequest request) {
+        if (request.getName() == null) {
             throw new MissingRequiredFieldResponse("name");
         }
-        websiteService.addPossibleWebsite(request.getName(), request.getPrettyName());
+        this.websiteService.addPossibleWebsite(request.getName(), request.getPrettyName());
         return new WebsiteAddedResponse();
     }
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Website> getWebsite(@PathVariable("id") String id){
-        Website website = websiteService.getWebsiteById(id);
-        if(website == null){
+    public ResponseEntity<Website> getWebsite(@PathVariable("id") String id) {
+        Website website = this.websiteService.getWebsiteById(id);
+        if (website == null) {
             throw new WebsiteNotFoundResponse();
         }
         return new GetWebsiteResponse(website);
     }
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<String> editWebsite(@PathVariable("id") String id, @RequestBody CreateWebsiteRequest request){
-        Website website = websiteService.getWebsiteById(id);
-        if(website == null){
+    public ResponseEntity<String> editWebsite(
+            @PathVariable("id") String id,
+            @RequestBody CreateWebsiteRequest request) {
+        Website website = this.websiteService.getWebsiteById(id);
+        if (website == null) {
             throw new WebsiteNotFoundResponse();
         }
-        websiteService.editWebsite(website, request.getName(), request.getPrettyName());
+        this.websiteService.editWebsite(website, request.getName(), request.getPrettyName());
         return new EditedWebsiteResponse();
     }
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<String> deleteWebsite(@PathVariable("id") String id){
-        groupWebsiteService.deleteGroupWebsiteByWebsite(websiteService.getWebsiteById(id));
-        userWebsiteService.deleteUserWebsiteByWebsite(websiteService.getWebsiteById(id));
-        websiteService.deleteWebsite(id);
+    public ResponseEntity<String> deleteWebsite(@PathVariable("id") String id) {
+        this.groupWebsiteService.deleteGroupWebsiteByWebsite(this.websiteService.getWebsiteById(id));
+        this.userWebsiteService.deleteUserWebsiteByWebsite(this.websiteService.getWebsiteById(id));
+        this.websiteService.deleteWebsite(id);
         return new WebsiteDeletedResponse();
     }
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<Website>> getAllWebsites(){
-        return new GetAllWebsitesResponse(websiteService.getAllWebsites());
+    public ResponseEntity<List<Website>> getAllWebsites() {
+        return new GetAllWebsitesResponse(this.websiteService.getAllWebsites());
     }
 
 }
