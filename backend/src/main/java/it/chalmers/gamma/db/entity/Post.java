c424e79d3668d664a021f52e4ed3fb5eaa8e8ee5
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2018-11-27 19:58:34 +0100

    Fix Post checkstyle violations

--- backend/src/main/java/it/chalmers/gamma/db/entity/Post.java
+++ backend/src/main/java/it/chalmers/gamma/db/entity/Post.java
@@ -1,12 +1,16 @@
 package it.chalmers.gamma.db.entity;
 
-
-import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
-import javax.persistence.*;
 import java.util.Objects;
 import java.util.UUID;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "post")
@@ -21,12 +25,13 @@ public class Post {
     @JoinColumn(name = "post_name")
     @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     private Text postName;
-    public Post(){
+    public Post() {
         this.postName = new Text();
         this.id = UUID.randomUUID();
     }
+
     public UUID getId() {
-        return id;
+        return this.id;
     }
 
     public void setId(UUID id) {
@@ -34,39 +39,43 @@ public class Post {
     }
 
     @JsonProperty("sv")
-    public void setSVPostName(String postName){
+    public void setSVPostName(String postName) {
         this.postName.setSv(postName);
     }
     @JsonProperty("en")
-    public void setENPostName(String postName){
+    public void setENPostName(String postName) {
         this.postName.setEn(postName);
     }
-   public String getSVPostName(){
-        return postName.getSv();
+    public String getSVPostName() {
+        return this.postName.getSv();
     }
-    public String getENPostName(){
-        return postName.getEn();
+    public String getENPostName() {
+        return this.postName.getEn();
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Post post = (Post) o;
-        return Objects.equals(id, post.id) &&
-                Objects.equals(postName, post.postName);
+        return Objects.equals(this.id, post.id)
+                && Objects.equals(this.postName, post.postName);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, postName);
+        return Objects.hash(this.id, this.postName);
     }
 
     @Override
     public String toString() {
-        return "Post{" +
-                "id=" + id +
-                ", postName='" + postName + '\'' +
-                '}';
+        return "Post{"
+                + "id=" + this.id
+                + ", postName='" + this.postName + '\''
+                + '}';
     }
 }
