fc323d439cdc1a648828e982cfcfd423239ecaf5
Theodor Angergård <portalprogramming@gmail.com>
2018-11-28 19:37:55 +0100

    Fixes GammaAplication pmd violations

--- backend/src/main/java/it/chalmers/gamma/GammaApplication.java
+++ backend/src/main/java/it/chalmers/gamma/GammaApplication.java
@@ -2,13 +2,12 @@ package it.chalmers.gamma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-
-import org.springframework.context.event.EventListener;
-
 
 @SpringBootApplication
-public class GammaApplication {
+public final class GammaApplication {
+
+    private GammaApplication() {}
+
     public static void main(String[] args) {
         SpringApplication.run(GammaApplication.class, args);
     }
