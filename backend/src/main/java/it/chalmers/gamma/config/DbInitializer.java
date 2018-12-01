cb2646c2fa803d0ffd09babbde1f70a34b96fa89
Gustav Engsmyre <gustav.engsmyre@gmail.com>
2018-12-02 00:34:35 +0100

    fix remaining checkStyle and PMD tests in main

--- backend/src/main/java/it/chalmers/gamma/config/DbInitializer.java
+++ backend/src/main/java/it/chalmers/gamma/config/DbInitializer.java
@@ -18,7 +18,6 @@ import java.time.Year;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 
