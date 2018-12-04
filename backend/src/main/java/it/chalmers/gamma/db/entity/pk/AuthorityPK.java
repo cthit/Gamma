a6290640965ce3d546a9c23249826210df3549c5
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2018-12-04 19:34:24 +0100

    Suppress serialize warning

--- backend/src/main/java/it/chalmers/gamma/db/entity/pk/AuthorityPK.java
+++ backend/src/main/java/it/chalmers/gamma/db/entity/pk/AuthorityPK.java
@@ -1,5 +1,6 @@
 package it.chalmers.gamma.db.entity.pk;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import it.chalmers.gamma.db.entity.FKITGroup;
 import it.chalmers.gamma.db.entity.Post;
 
@@ -11,6 +12,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Embeddable
+@SuppressFBWarnings(justification="Fields should be serializable", value="SE_BAD_FIELD")
 public class AuthorityPK implements Serializable {
 
     @ManyToOne
