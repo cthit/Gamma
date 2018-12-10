f7c615724d08cd549d110f1d8f7eae5150acc5f9
Theodor Angergård <portalprogramming@gmail.com>
2018-12-10 06:44:34 +0100

    Groups' screen now makes requests on their own

--- frontend/src/use-cases/groups/screens/show-all-groups/ShowAllGroups.screen.container.jsx
+++ frontend/src/use-cases/groups/screens/show-all-groups/ShowAllGroups.screen.container.jsx
@@ -1,12 +1,21 @@
 import { connect } from "react-redux";
 
 import ShowAllGroups from "./ShowAllGroups.screen";
+import { createGetGroupsAction } from "../../../../api/groups/action-creator.groups.api";
+import {
+    gammaLoadingFinished,
+    gammaLoadingStart
+} from "../../../../app/views/gamma-loading/GammaLoading.view.action-creator";
 
 const mapStateToProps = (state, ownProps) => ({
     groups: state.groups
 });
 
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({
+    groupsLoad: () => dispatch(createGetGroupsAction()),
+    gammaLoadingFinished: () => dispatch(gammaLoadingFinished()),
+    gammaLoadingStart: () => dispatch(gammaLoadingStart())
+});
 
 export default connect(
     mapStateToProps,
