0681ae52904d50886171bbe2a6ae9bc373283a40
Gustav Engsmyre <gustav.engsmyre@gmail.com>
2018-12-15 14:24:22 +0100

    fix Checkstyle for GroupAlreadyExistsResponse.java

--- backend/src/main/java/it/chalmers/gamma/response/GroupAlreadyExistsResponse.java
+++ backend/src/main/java/it/chalmers/gamma/response/GroupAlreadyExistsResponse.java
@@ -2,10 +2,8 @@ package it.chalmers.gamma.response;
 
 import org.springframework.http.HttpStatus;
 
-public class GroupAlreadyExistsResponse extends CustomResponseStatusException
-{
+public class GroupAlreadyExistsResponse extends CustomResponseStatusException {
     public GroupAlreadyExistsResponse() {
         super(HttpStatus.UNPROCESSABLE_ENTITY, "GROUP_ALREADY_EXISTS");
     }
-
 }
