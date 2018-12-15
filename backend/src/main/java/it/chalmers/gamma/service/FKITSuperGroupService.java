0558a47f65c21b9c195460f0c950c42b986c8685
Gustav Engsmyre <gustav.engsmyre@gmail.com>
2018-12-15 13:25:42 +0100

    fix checkstyle violations in FKITSuperGroupService

--- backend/src/main/java/it/chalmers/gamma/service/FKITSuperGroupService.java
+++ backend/src/main/java/it/chalmers/gamma/service/FKITSuperGroupService.java
@@ -3,47 +3,48 @@ package it.chalmers.gamma.service;
 import it.chalmers.gamma.db.entity.FKITSuperGroup;
 import it.chalmers.gamma.db.repository.FKITSuperGroupRepository;
 import it.chalmers.gamma.requests.CreateSuperGroupRequest;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.stereotype.Service;
+
 @Service
 public class FKITSuperGroupService {
-    FKITSuperGroupRepository repository;
+    private final FKITSuperGroupRepository repository;
 
-    public FKITSuperGroupService(FKITSuperGroupRepository repository){
+    public FKITSuperGroupService(FKITSuperGroupRepository repository) {
         this.repository = repository;
     }
 
-    public FKITSuperGroup createSuperGroup(CreateSuperGroupRequest request){
+    public FKITSuperGroup createSuperGroup(CreateSuperGroupRequest request) {
         FKITSuperGroup group = new FKITSuperGroup();
         group.setName(request.getName());
         group.setPrettyName(request.getPrettyName() == null ? request.getName() : request.getPrettyName());
         group.setType(request.getType());
-        return repository.save(group);
+        return this.repository.save(group);
     }
 
-    public FKITSuperGroup getGroup(UUID id){
-        return repository.getById(id);
+    public FKITSuperGroup getGroup(UUID id) {
+        return this.repository.getById(id);
     }
 
-    public boolean groupExists(String name){
-        return repository.existsByName(name);
+    public boolean groupExists(String name) {
+        return this.repository.existsByName(name);
     }
-    public boolean groupExists(UUID id){
-        return repository.existsById(id);
-    }
-
-    public void removeGroup(UUID id){
-        repository.deleteById(id);
-    }
-    public List<FKITSuperGroup> getAllGroups(){
-        return repository.findAll();
+    public boolean groupExists(UUID id) {
+        return this.repository.existsById(id);
     }
 
-    public void updateSuperGroup(UUID id, CreateSuperGroupRequest request){
-        FKITSuperGroup group = repository.getById(id);
+    public void removeGroup(UUID id) {
+        this.repository.deleteById(id);
+    }
+    public List<FKITSuperGroup> getAllGroups() {
+        return this.repository.findAll();
+    }
+
+    public void updateSuperGroup(UUID id, CreateSuperGroupRequest request) {
+        FKITSuperGroup group = this.repository.getById(id);
         group.setType(request.getType() == null ? group.getType() : request.getType());
         group.setName(request.getName() == null ? group.getName() : request.getName());
         group.setPrettyName(request.getPrettyName() == null ? group.getPrettyName() : request.getPrettyName());
