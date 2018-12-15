7b27b8b739dfd4f00084afec5bda556eeae0c802
Gustav Engsmyre <gustav.engsmyre@gmail.com>
2018-12-15 14:16:01 +0100

    fix checkstyle in AuthorityLevel.java

--- backend/src/main/java/it/chalmers/gamma/db/entity/AuthorityLevel.java
+++ backend/src/main/java/it/chalmers/gamma/db/entity/AuthorityLevel.java
@@ -1,5 +1,8 @@
 package it.chalmers.gamma.db.entity;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import java.util.Objects;
 import java.util.UUID;
 
@@ -8,8 +11,6 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import org.springframework.security.core.GrantedAuthority;
 
 @Entity
