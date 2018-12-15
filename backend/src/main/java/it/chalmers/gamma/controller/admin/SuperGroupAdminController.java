7f03e802054127f6d62b34703afcd7db03525040
Gustav Engsmyre <gustav.engsmyre@gmail.com>
2018-12-15 13:12:43 +0100

    fix checkstyle violations in SuperGroupAdminController.java

--- backend/src/main/java/it/chalmers/gamma/controller/admin/SuperGroupAdminController.java
+++ backend/src/main/java/it/chalmers/gamma/controller/admin/SuperGroupAdminController.java
@@ -2,13 +2,25 @@ package it.chalmers.gamma.controller.admin;
 
 import it.chalmers.gamma.db.entity.FKITSuperGroup;
 import it.chalmers.gamma.requests.CreateSuperGroupRequest;
-import it.chalmers.gamma.response.*;
+import it.chalmers.gamma.response.FKITSuperGroupCreatedResponse;
+import it.chalmers.gamma.response.GetGroupsResponse;
+import it.chalmers.gamma.response.GetSuperGroupResponse;
+import it.chalmers.gamma.response.GroupAlreadyExistsResponse;
+import it.chalmers.gamma.response.GroupDeletedResponse;
+import it.chalmers.gamma.response.GroupDoesNotExistResponse;
+import it.chalmers.gamma.response.GroupEditedResponse;
 import it.chalmers.gamma.service.FKITSuperGroupService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/admin/superGroups")       // What should this URL be?
@@ -16,43 +28,43 @@ public class SuperGroupAdminController {
     private final FKITSuperGroupService fkitSuperGroupService;
 
 
-    public SuperGroupAdminController(FKITSuperGroupService fkitSuperGroupService){
+    public SuperGroupAdminController(FKITSuperGroupService fkitSuperGroupService) {
         this.fkitSuperGroupService = fkitSuperGroupService;
 
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<FKITSuperGroup> createSuperGroup(@RequestBody CreateSuperGroupRequest request){
-            if(fkitSuperGroupService.groupExists(request.getName())){
-                throw new GroupAlreadyExistsResponse();
-            }
-            FKITSuperGroup group = fkitSuperGroupService.createSuperGroup(request);
-            return new FKITSuperGroupCreatedResponse(group);
+    public ResponseEntity<FKITSuperGroup> createSuperGroup(@RequestBody CreateSuperGroupRequest request) {
+        if (this.fkitSuperGroupService.groupExists(request.getName())) {
+            throw new GroupAlreadyExistsResponse();
         }
+        FKITSuperGroup group = this.fkitSuperGroupService.createSuperGroup(request);
+        return new FKITSuperGroupCreatedResponse(group);
+    }
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<FKITSuperGroup>> getAllSuperGroups(){
-        return new GetGroupsResponse(fkitSuperGroupService.getAllGroups());
+    public ResponseEntity<List<FKITSuperGroup>> getAllSuperGroups() {
+        return new GetGroupsResponse(this.fkitSuperGroupService.getAllGroups());
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<FKITSuperGroup> getSuperGroup(@PathVariable("id") String id){
-        return new GetSuperGroupResponse(fkitSuperGroupService.getGroup(UUID.fromString(id)));
+    public ResponseEntity<FKITSuperGroup> getSuperGroup(@PathVariable("id") String id) {
+        return new GetSuperGroupResponse(this.fkitSuperGroupService.getGroup(UUID.fromString(id)));
     }
 
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<String> removeSuperGroup(@PathVariable("id") String id){
-        fkitSuperGroupService.removeGroup(UUID.fromString(id));
+    public ResponseEntity<String> removeSuperGroup(@PathVariable("id") String id) {
+        this.fkitSuperGroupService.removeGroup(UUID.fromString(id));
         return new GroupDeletedResponse();
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public ResponseEntity<String> updateSuperGroup(@PathVariable("id") String id,
-                                                   @RequestBody CreateSuperGroupRequest request){
-        if(!fkitSuperGroupService.groupExists(UUID.fromString(id))){
+                                                   @RequestBody CreateSuperGroupRequest request) {
+        if (!this.fkitSuperGroupService.groupExists(UUID.fromString(id))) {
             throw new GroupDoesNotExistResponse();
         }
-        fkitSuperGroupService.updateSuperGroup(UUID.fromString(id), request);
+        this.fkitSuperGroupService.updateSuperGroup(UUID.fromString(id), request);
         return new GroupEditedResponse();
     }
 }
