c869b5115942f5e7f475ee246a1957732d03d1c1
Gustav Engsmyre <gustav.engsmyre@gmail.com>
2018-12-28 22:51:52 +0100

    Add Validation for JwtController

--- backend/src/main/java/it/chalmers/gamma/controller/JwtController.java
+++ backend/src/main/java/it/chalmers/gamma/controller/JwtController.java
@@ -2,13 +2,18 @@ package it.chalmers.gamma.controller;
 
 import it.chalmers.gamma.jwt.JwtTokenProvider;
 import it.chalmers.gamma.requests.ValidateJwtRequest;
+import it.chalmers.gamma.response.InputValidationFailedResponse;
 import it.chalmers.gamma.response.ValidJwtResponse;
 
+import it.chalmers.gamma.util.InputValidationUtils;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/validate_jwt")
@@ -21,7 +26,11 @@ public final class JwtController {
     }
 
     @PostMapping
-    public ResponseEntity<Boolean> tokenIsValid(@RequestBody ValidateJwtRequest validateJwtRequest) {
+    public ResponseEntity<Boolean> tokenIsValid(@Valid @RequestBody ValidateJwtRequest validateJwtRequest,
+                                                BindingResult result) {
+        if(result.hasErrors()){
+            throw new InputValidationFailedResponse(InputValidationUtils.getErrorMessages(result.getAllErrors()));
+        }
         return new ValidJwtResponse(this.tokenProvider.validateToken(validateJwtRequest.getJwt()));
     }
 }
