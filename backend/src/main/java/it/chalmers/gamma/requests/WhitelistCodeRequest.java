e35348396dd57b45d6aa607047d4bf39144a29cb
Gustav Engsmyre <gustav.engsmyre@gmail.com>
2018-12-29 23:09:26 +0100

    Fix Checkstyle for WhitelistCodeRequest

--- backend/src/main/java/it/chalmers/gamma/requests/WhitelistCodeRequest.java
+++ backend/src/main/java/it/chalmers/gamma/requests/WhitelistCodeRequest.java
@@ -1,7 +1,7 @@
 package it.chalmers.gamma.requests;
 
-import javax.validation.constraints.NotEmpty;
 import java.util.Objects;
+import javax.validation.constraints.NotEmpty;
 
 public class WhitelistCodeRequest {
     @NotEmpty(message = "CID_MUST_BE_PROVIDED")
