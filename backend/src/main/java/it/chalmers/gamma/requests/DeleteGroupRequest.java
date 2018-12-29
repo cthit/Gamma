aadb0297dfb1cdfa75cb338f5b4ef6a3f73b975b
Gustav Engsmyre <gustav.engsmyre@gmail.com>
2018-12-29 23:07:12 +0100

    Fix Checkstyle for DeleteGroupRequest

--- backend/src/main/java/it/chalmers/gamma/requests/DeleteGroupRequest.java
+++ backend/src/main/java/it/chalmers/gamma/requests/DeleteGroupRequest.java
@@ -1,7 +1,7 @@
 package it.chalmers.gamma.requests;
 
-import javax.validation.constraints.NotNull;
 import java.util.Objects;
+import javax.validation.constraints.NotNull;
 
 public class DeleteGroupRequest {       // Unsure about this one
     @NotNull(message = "GROUP_MUST_BE_PROVIDED")
