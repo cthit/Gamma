e96512abe9523716394a5d0d04c9c79bd2fdc5f5
portals <theodor.ang.98@gmail.com>
2018-12-29 12:58:26 +0100

    Adds bracets to if statement in ITClientAdminController

--- backend/src/main/java/it/chalmers/gamma/controller/admin/ITClientAdminController.java
+++ backend/src/main/java/it/chalmers/gamma/controller/admin/ITClientAdminController.java
@@ -2,13 +2,23 @@ package it.chalmers.gamma.controller.admin;
 
 import it.chalmers.gamma.db.entity.ITClient;
 import it.chalmers.gamma.requests.AddITClientRequest;
-import it.chalmers.gamma.response.*;
+import it.chalmers.gamma.response.EditedClientResponse;
+import it.chalmers.gamma.response.GetAllClientsResponse;
+import it.chalmers.gamma.response.GetITClient;
+import it.chalmers.gamma.response.ITClientAdded;
+import it.chalmers.gamma.response.ITClientRemovedResponse;
+import it.chalmers.gamma.response.NoSuchClientExistsResponse;
 import it.chalmers.gamma.service.ITClientService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/admin/auth")
@@ -24,22 +34,24 @@ public class ITClientAdminController {
         return new ITClientAdded();
     }
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<ITClient>> getAllOauthClients(){
+    public ResponseEntity<List<ITClient>> getAllOauthClients() {
         return new GetAllClientsResponse(itClientService.getAllClients());
     }
     @RequestMapping(method = RequestMethod.GET, value = "/{id}")
-    public ResponseEntity<ITClient> getClient(@PathVariable("id") String id){
+    public ResponseEntity<ITClient> getClient(@PathVariable("id") String id) {
         ITClient client = itClientService.getITClient(UUID.fromString(id));
         return new GetITClient(client);
     }
     @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
-    public ResponseEntity<String> removeClient(@PathVariable("id") String id){
-        if(itClientService.clientExists(UUID.fromString(id)))
-        itClientService.removeITClient(UUID.fromString(id));
+    public ResponseEntity<String> removeClient(@PathVariable("id") String id) {
+        if(itClientService.clientExists(UUID.fromString(id))) {
+            itClientService.removeITClient(UUID.fromString(id));
+        }
         return new ITClientRemovedResponse();
     }
     @RequestMapping(method = RequestMethod.PUT, value = "/{id}")
-    public ResponseEntity<String> editClient(@PathVariable("id") String id, @RequestBody AddITClientRequest request){
+    public ResponseEntity<String> editClient(
+            @PathVariable("id") String id, @RequestBody AddITClientRequest request) {
         if(itClientService.clientExists(UUID.fromString(id))){
             throw new NoSuchClientExistsResponse();
         }
