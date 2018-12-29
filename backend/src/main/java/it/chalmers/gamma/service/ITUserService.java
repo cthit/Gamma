683187d3bdd1636ea91ef9f1b4040b1ffe124253
portals <theodor.ang.98@gmail.com>
2018-12-29 13:16:07 +0100

    Removes unused imports in ITUserService

--- backend/src/main/java/it/chalmers/gamma/service/ITUserService.java
+++ backend/src/main/java/it/chalmers/gamma/service/ITUserService.java
@@ -11,12 +11,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
