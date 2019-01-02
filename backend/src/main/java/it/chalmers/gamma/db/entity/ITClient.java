a06f5a3c3321be1149953d0df0d615b3d56cda75
Gustav Engsmyre <gustav.engsmyre@gmail.com>
2019-01-02 16:48:03 +0100

    Volkswagen has recruited Portals, he is one of them now. Also Spotbugs fix

--- backend/src/main/java/it/chalmers/gamma/db/entity/ITClient.java
+++ backend/src/main/java/it/chalmers/gamma/db/entity/ITClient.java
@@ -2,6 +2,8 @@ package it.chalmers.gamma.db.entity;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
 import java.time.Instant;
 import java.util.Collection;
 import java.util.HashMap;
@@ -20,7 +22,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.provider.ClientDetails;
