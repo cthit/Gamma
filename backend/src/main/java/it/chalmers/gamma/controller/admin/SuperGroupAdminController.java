c4831947d101694a143b04a1701f6c3d92ea24b9
Gustav Engsmyre <gustav.engsmyre@gmail.com>
2019-01-12 21:24:10 +0100

    fix PMD violations in SuperGroupAdminController

--- backend/src/main/java/it/chalmers/gamma/controller/admin/SuperGroupAdminController.java
+++ backend/src/main/java/it/chalmers/gamma/controller/admin/SuperGroupAdminController.java
@@ -1,22 +1,16 @@
 package it.chalmers.gamma.controller.admin;
 
-import it.chalmers.gamma.db.entity.FKITGroup;
 import it.chalmers.gamma.db.entity.FKITSuperGroup;
 import it.chalmers.gamma.requests.CreateSuperGroupRequest;
 import it.chalmers.gamma.response.FKITSuperGroupCreatedResponse;
-import it.chalmers.gamma.response.GetGroupsResponse;
-import it.chalmers.gamma.response.GetSuperGroupResponse;
 import it.chalmers.gamma.response.GroupAlreadyExistsResponse;
 import it.chalmers.gamma.response.GroupDeletedResponse;
 import it.chalmers.gamma.response.GroupDoesNotExistResponse;
 import it.chalmers.gamma.response.GroupEditedResponse;
-import it.chalmers.gamma.response.GroupsResponse;
 import it.chalmers.gamma.response.InputValidationFailedResponse;
-import it.chalmers.gamma.service.FKITService;
 import it.chalmers.gamma.service.FKITSuperGroupService;
 import it.chalmers.gamma.util.InputValidationUtils;
 
-import java.util.List;
 import java.util.UUID;
 
 import javax.validation.Valid;
@@ -33,12 +27,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/admin/superGroups")       // What should this URL be?
 public class SuperGroupAdminController {
     private final FKITSuperGroupService fkitSuperGroupService;
-    private final FKITService fkitService;
 
 
-    public SuperGroupAdminController(FKITSuperGroupService fkitSuperGroupService, FKITService fkitService) {
+    public SuperGroupAdminController(FKITSuperGroupService fkitSuperGroupService) {
         this.fkitSuperGroupService = fkitSuperGroupService;
-        this.fkitService = fkitService;
     }
 
     @RequestMapping(method = RequestMethod.POST)
