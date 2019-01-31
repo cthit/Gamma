584350b77c216e00e86a63262ee95c150b4671b6
portals <theodor.ang.98@gmail.com>
2019-01-31 14:29:36 +0100

    Changes to api: websites

--- frontend/src/api/websites/post.websites.api.js
+++ frontend/src/api/websites/post.websites.api.js
@@ -1,6 +1,6 @@
 import { postRequest } from "../utils/api";
-import { WEBSITES_ENDPOINT } from "../utils/endpoints";
+import { ADMIN_WEBSITES_ENDPOINT } from "../utils/endpoints";
 
 export function addWebsite(websiteData) {
-    return postRequest(WEBSITES_ENDPOINT, websiteData);
+    return postRequest(ADMIN_WEBSITES_ENDPOINT, websiteData);
 }
