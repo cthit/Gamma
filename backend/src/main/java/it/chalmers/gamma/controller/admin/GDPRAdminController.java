18856fd322a54f3a39988bf947f23b79ea093fb5
portals <theodor.ang.98@gmail.com>
2019-02-05 22:22:49 +0100

    Resolves checkstyle issues in GDPRAdminController

--- backend/src/main/java/it/chalmers/gamma/controller/admin/GDPRAdminController.java
+++ backend/src/main/java/it/chalmers/gamma/controller/admin/GDPRAdminController.java
@@ -6,6 +6,11 @@ import it.chalmers.gamma.response.InputValidationFailedResponse;
 import it.chalmers.gamma.response.UserNotFoundResponse;
 import it.chalmers.gamma.service.ITUserService;
 import it.chalmers.gamma.util.InputValidationUtils;
+
+import java.util.UUID;
+
+import javax.validation.Valid;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,9 +18,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
-import java.util.UUID;
 
 @RestController()
 @RequestMapping("/admin/gdpr")
@@ -30,11 +32,11 @@ public class GDPRAdminController {
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public ResponseEntity<String> setGDPRStatus(@PathVariable("id") String id,
                                                 @Valid @RequestBody ChangeGDPRStatusRequest request,
-                                                BindingResult result){
+                                                BindingResult result) {
         if (result.hasErrors()) {
             throw new InputValidationFailedResponse(InputValidationUtils.getErrorMessages(result.getAllErrors()));
         }
-        if (!itUserService.userExists(UUID.fromString(id))) {
+        if (!this.itUserService.userExists(UUID.fromString(id))) {
             throw new UserNotFoundResponse();
         }
         this.itUserService.editGdpr(UUID.fromString(id), request.isGdpr());
