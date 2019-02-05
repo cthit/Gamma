c0ce5694c031a9018b1ae49d443e0e1587aceb81
portals <theodor.ang.98@gmail.com>
2019-02-05 22:28:34 +0100

    Resolves checkstyle issues with ITClient

--- backend/src/main/java/it/chalmers/gamma/db/entity/ITClient.java
+++ backend/src/main/java/it/chalmers/gamma/db/entity/ITClient.java
@@ -2,6 +2,8 @@ package it.chalmers.gamma.db.entity;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
 import java.time.Instant;
 import java.util.Collection;
 import java.util.HashMap;
@@ -24,7 +26,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.provider.ClientDetails;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 @Entity
 @Table(name = "itclient")
