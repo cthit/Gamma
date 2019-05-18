5d1a92e51e82dbc7e179cb9ada2a98239dbf48c4
Gustav Engsmyre <gustav.engsmyre@gmail.com>
2019-05-18 17:07:51 +0200

    fix PMD complaints

--- backend/src/main/java/it/chalmers/gamma/filter/AuthenticationFilter.java
+++ backend/src/main/java/it/chalmers/gamma/filter/AuthenticationFilter.java
@@ -17,11 +17,9 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.constraints.NotNull;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.lang.NonNullApi;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
