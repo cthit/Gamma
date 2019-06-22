d1c02d74a4edb0849ad2a09d79ce47013361ce74
gurr1 <gustav.engsmyre@gmail.com>
2019-06-22 15:43:15 +0200

    Add view API key details functionality to frontend

--- frontend/src/use-cases/api-keys/ApiKeys.jsx
+++ frontend/src/use-cases/api-keys/ApiKeys.jsx
@@ -2,11 +2,13 @@ import { DigitLayout } from "@cthit/react-digit-components";
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import ShowAllApiKeys from "./screens/show-all-api-keys";
+import ShowApiKeyDetails from "./screens/show-api-key-details";
 
 const ApiKeys = () => (
     <DigitLayout.Fill>
         <Switch>
             <Route path={"/api_keys"} exact component={ShowAllApiKeys}/>
+            <Route path={"/api_keys/:id"} exact component={ShowApiKeyDetails}/>
         </Switch>
     </DigitLayout.Fill>
 );
