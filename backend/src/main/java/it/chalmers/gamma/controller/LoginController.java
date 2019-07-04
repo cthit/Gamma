3f59ab1b7b9fd1ccc591a32243cd72d63d8b5c39
Gustav Engsmyre <gustav.engsmyre@gmail.com>
2019-07-04 18:03:45 +0200

    Switch Thymeleaf to use environment variables

--- backend/src/main/java/it/chalmers/gamma/controller/LoginController.java
+++ backend/src/main/java/it/chalmers/gamma/controller/LoginController.java
@@ -1,5 +1,6 @@
 package it.chalmers.gamma.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,11 +8,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+    @Value("${application.frontend-client-details.successful-login-uri:}")
+    private String baseFrontendUrl;
 
     @GetMapping("/login")
     public String getLogin(@RequestParam(value = "error", required = false) String error,
                            @RequestParam(value = "logout", required = false) String logout,
                            Model model) {
+        model.addAttribute("createAccountUrl", baseFrontendUrl + "/create-account");
+        model.addAttribute("forgotPasswordUrl", baseFrontendUrl + "/reset-password");
+        model.addAttribute("error", error);
+        model.addAttribute("logout", logout);
+
         return "login";
     }
 
