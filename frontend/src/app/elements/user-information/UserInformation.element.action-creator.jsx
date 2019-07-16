3227b39c49a81d4a99abb9836a5e11ebe3704c23
gurr1 <gustav.engsmyre@gmail.com>
2019-07-16 16:29:40 +0200

    fix some problems with production environment

--- frontend/src/app/elements/user-information/UserInformation.element.action-creator.jsx
+++ frontend/src/app/elements/user-information/UserInformation.element.action-creator.jsx
@@ -24,7 +24,7 @@ export function userUpdateMe() {
                 axios
                     .get(
                         (process.env.REACT_APP_BACKEND_URL ||
-                            "http://localhost:8081") + "/api/users/me",
+                            "http://localhost:8081/api") + "/users/me",
                         {
                             headers: {
                                 Authorization: "Bearer " + token()
