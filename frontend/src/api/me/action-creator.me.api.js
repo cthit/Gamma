163a619c582013e2ec7992d62af1badf893adbf3
gurr1 <gustav.engsmyre@gmail.com>
2019-07-21 16:58:22 +0200

    add API code for /me

---
+++ frontend/src/api/me/action-creator.me.api.js
@@ -0,0 +1,119 @@
+import {
+    ME_CHANGE_FAILED,
+    ME_CHANGE_SUCCESSFULLY,
+    ME_GET_LOADING,
+    ME_GET_FAILED,
+    ME_GET_SUCCESSFULLY,
+    ME_CHANGE_PASSWORD_FAILED,
+    ME_CHANGE_PASSWORD_SUCCESSFULLY
+} from "actions.me.api";
+
+import { editMe, editPassword } from "./put.me.api";
+import { getMe } from "./get.me.api";
+
+import { requestPromise } from "../utils/requestPromise";
+import { failed, loading, successfully } from "../utils/simpleActionCreators";
+
+export function createGetMeAction() {
+    return requestPromise(
+        () => {
+            return getMe();
+        },
+        meGetLoading,
+        meGetSuccessfully,
+        meGetFailed()
+    );
+}
+
+export function createEditMeAction(userData) {
+    return dispatch => {
+        return new Promise((resolve, reject) => {
+            editMe(userData)
+                .then(response => {
+                    dispatch(meChangePasswordSuccessfully());
+                    resolve(response.data);
+                })
+                .catch(error => {
+                    dispatch(meChangeFailed(error));
+                    reject(error);
+                });
+        });
+    };
+}
+
+export function createChangeMePasswordAction(passwordData) {
+    return dispatch => {
+        return new Promise((resolve, reject) => {
+            editPassword(passwordData)
+                .then(response => {
+                    dispatch(meChangePasswordSuccessfully());
+                    resolve(response);
+                })
+                .catch(error => {
+                    dispatch(meChangePasswordFailed());
+                    reject(error);
+                });
+        });
+    };
+}
+
+function meChangeFailed(error) {
+    return {
+        type: ME_CHANGE_FAILED,
+        error: true,
+        payload: {
+            error: error
+        }
+    };
+}
+
+function meChangeSuccessfully() {
+    return {
+        type: ME_CHANGE_SUCCESSFULLY,
+        error: false
+    };
+}
+
+function meGetLoading() {
+    return {
+        type: ME_GET_LOADING,
+        error: false
+    };
+}
+
+function meGetSuccessfully(response) {
+    return {
+        type: ME_GET_SUCCESSFULLY,
+        error: false,
+        payload: {
+            data: response.data
+        }
+    };
+}
+
+function meGetFailed(error) {
+    return {
+        type: ME_GET_FAILED,
+        error: true,
+        payload: {
+            error: error
+        }
+    };
+}
+
+function meChangePasswordSuccessfully() {
+    return {
+        type: ME_CHANGE_PASSWORD_SUCCESSFULLY,
+        error: false
+    };
+}
+
+function meChangePasswordFailed(error) {
+    return {
+        type: ME_CHANGE_PASSWORD_FAILED,
+        error: true,
+        payload: {
+            error: error
+        }
+    };
+}
