ff001cb53639c6312ba88067c396e985744bc218
gurr1 <gustav.engsmyre@gmail.com>
2019-08-01 03:07:52 +0200

    Add ability to delete self from frontend

--- frontend/src/use-cases/me/views/my-account/MyAccount.view.container.jsx
+++ frontend/src/use-cases/me/views/my-account/MyAccount.view.container.jsx
@@ -1,13 +1,15 @@
 import { connect } from "react-redux";
 import MyAccount from "./MyAccount.view";
 import { gammaLoadingFinished } from "../../../../app/views/gamma-loading/GammaLoading.view.action-creator";
+import { DigitRedirectActions } from "@cthit/react-digit-components";
 
 const mapStateToProps = (state, ownProps) => ({
     me: state.user
 });
 
 const mapDispatchToProps = dispatch => ({
-    gammaLoadingFinished: () => dispatch(gammaLoadingFinished())
+    gammaLoadingFinished: () => dispatch(gammaLoadingFinished()),
+    redirectTo: to => dispatch(DigitRedirectActions.digitRedirectTo(to))
 });
 
 export default connect(
