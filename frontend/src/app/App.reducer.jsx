9650252ba1e2412de722e947676fd250a6d8fbdb
Theodor Angergård <portalprogramming@gmail.com>
2019-08-05 13:27:15 +0200

    Super group now uses DigitCRUD

--- frontend/src/app/App.reducer.jsx
+++ frontend/src/app/App.reducer.jsx
@@ -4,7 +4,6 @@ import { users } from "../use-cases/users/Users.reducer";
 import { user } from "./elements/user-information/UserInformation.element.reducer";
 import { loading } from "./views/gamma-loading/GammaLoading.view.reducer";
 import { gammaIntegration } from "./views/gamma-integration/GammaIntegration.view.reducer";
-import { superGroups } from "../use-cases/super-groups/SuperGroups.reducer";
 import { gdpr } from "../use-cases/gdpr/Gdpr.reducer";
 
 export const rootReducer = {
@@ -14,6 +13,5 @@ export const rootReducer = {
     groups,
     loading,
     gammaIntegration,
-    superGroups,
     gdpr
 };
--- frontend/src/use-cases/super-groups/SuperGroups.jsx
+++ frontend/src/use-cases/super-groups/SuperGroups.jsx
@@ -1,23 +1,122 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import ShowAllSuperGroups from "./screens/show-all-super-groups";
-import ShowSuperGroupDetails from "./screens/show-super-group-details";
-import CreateNewSuperGroup from "./screens/create-new-super-group";
-import EditSuperGroup from "./screens/edit-super-group-details";
+import React, { useEffect } from "react";
+import {
+    EMAIL,
+    NAME,
+    PRETTY_NAME,
+    TYPE,
+    TYPE_BOARD,
+    TYPE_COMMITTEE,
+    TYPE_SOCIETY
+} from "../../api/super-groups/props.super-groups.api";
+import * as yup from "yup";
+import {
+    DigitSelect,
+    DigitTextField,
+    useDigitTranslations,
+    DigitCRUD
+} from "@cthit/react-digit-components";
+import translations from "./SuperGroups.translations";
+import { useDispatch } from "react-redux";
+import { gammaLoadingFinished } from "../../app/views/gamma-loading/GammaLoading.view.action-creator";
+import {
+    getSuperGroup,
+    getSuperGroups
+} from "../../api/super-groups/get.super-groups.api";
+import { addSuperGroup } from "../../api/super-groups/post.super-groups.api";
+import { deleteSuperGroup } from "../../api/super-groups/delete.super-groups.api";
+import { editSuperGroup } from "../../api/super-groups/put.super-groups.api";
 
-const SuperGroups = () => (
-    <Switch>
-        <Route
-            path="/super-groups/:superGroupId/edit"
-            component={EditSuperGroup}
+function generateValidationSchema(text) {
+    const schema = {};
+    schema[NAME] = yup.string().required();
+    schema[PRETTY_NAME] = yup.string().required();
+    schema[TYPE] = yup.string().required();
+
+    return yup.object().shape(schema);
+}
+
+function generateEditComponentData(text) {
+    const componentData = {};
+
+    componentData[NAME] = {
+        component: DigitTextField,
+        componentProps: {
+            upperLabel: text.Name,
+            outlined: true
+        }
+    };
+
+    componentData[PRETTY_NAME] = {
+        component: DigitTextField,
+        componentProps: {
+            upperLabel: text.PrettyName,
+            outlined: true
+        }
+    };
+
+    componentData[EMAIL] = {
+        component: DigitTextField,
+        componentProps: {
+            upperLabel: text.Email,
+            outlined: true
+        }
+    };
+
+    const typeValueToTextMap = {};
+    typeValueToTextMap[TYPE_SOCIETY] = text.Society;
+    typeValueToTextMap[TYPE_COMMITTEE] = text.Committee;
+    typeValueToTextMap[TYPE_BOARD] = text.Board;
+
+    componentData[TYPE] = {
+        component: DigitSelect,
+        componentProps: {
+            upperLabel: text.Type,
+            valueToTextMap: typeValueToTextMap,
+            outlined: true
+        }
+    };
+
+    return componentData;
+}
+
+const SuperGroups = () => {
+    const [text] = useDigitTranslations(translations);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(gammaLoadingFinished());
+    }, []);
+
+    return (
+        <DigitCRUD
+            name={"superGroups"}
+            path={"/super-groups"}
+            readAllRequest={getSuperGroups}
+            readOneRequest={getSuperGroup}
+            createRequest={addSuperGroup}
+            deleteRequest={deleteSuperGroup}
+            updateRequest={editSuperGroup}
+            keysOrder={[NAME, PRETTY_NAME, TYPE, EMAIL]}
+            keysText={{
+                name: text.Name,
+                prettyName: text.PrettyName,
+                type: text.Type,
+                email: text.Email
+            }}
+            formComponentData={generateEditComponentData(text)}
+            formInitialValues={{
+                name: "",
+                prettyName: "",
+                type: TYPE_SOCIETY,
+                email: ""
+            }}
+            formValidationSchema={generateValidationSchema(text)}
+            tableProps={{
+                titleText: text.SuperGroups,
+                search: true
+            }}
+            idProp={"id"}
         />
-        <Route path="/super-groups/add" component={CreateNewSuperGroup} />
-        <Route
-            path="/super-groups/:superGroupId"
-            component={ShowSuperGroupDetails}
-        />
-        <Route path="/super-groups" component={ShowAllSuperGroups} />
-    </Switch>
-);
+    );
+};
 
 export default SuperGroups;
