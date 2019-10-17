f9ae0a6bb393c4fc520b43ac9544f3d667563f5d
Theodor Angergård <portalprogramming@gmail.com>
2019-10-17 02:27:48 +0200

    Removes users reducer

--- frontend/src/app/App.reducer.jsx
+++ frontend/src/app/App.reducer.jsx
@@ -1,5 +1,4 @@
 import { createAccount } from "../use-cases/create-account/CreateAccount.reducer";
-import { users } from "../use-cases/users/Users.reducer";
 import { user } from "./elements/user-information/UserInformation.element.reducer";
 import { loading } from "./views/gamma-loading/GammaLoading.view.reducer";
 import { gammaIntegration } from "./views/gamma-integration/GammaIntegration.view.reducer";
@@ -8,7 +7,6 @@ import { gdpr } from "../use-cases/gdpr/Gdpr.reducer";
 export const rootReducer = {
     createAccount,
     user,
-    users,
     loading,
     gammaIntegration,
     gdpr
