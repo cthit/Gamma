c795c392b9212e571e514346dde0be2e642b7072
Theodor Angergård <portalprogramming@gmail.com>
2019-12-09 19:10:44 +0100

    Deletes unused action creators and actions files in api

--- frontend/src/api/groups/actions.groups.api.js
+++ frontend/src/api/groups/actions.groups.api.js
@@ -1,15 +1,3 @@
-export const GROUPS_LOAD_SUCCESSFULLY = "groups_load_successfully";
-export const GROUPS_LOAD_FAILED = "groups_load_failed";
-
-export const GROUPS_ADD_SUCCESSFULLY = "groups_add_successfully";
-export const GROUPS_ADD_FAILED = "groups_add_failed";
-
-export const GROUPS_DELETE_SUCCESSFULLY = "groups_delete_successfully";
-export const GROUPS_DELETE_FAILED = "groups_delete_failed";
-
-export const GROUPS_CHANGE_SUCCESSFULLY = "groups_change_successfully";
-export const GROUPS_CHANGE_FAILED = "groups_change_failed";
-
 export const GROUPS_ADD_USER_SUCCESSFULLY = "groups_add_user_successfully";
 export const GROUPS_ADD_USER_FAILED = "groups_add_user_failed";
 export const GROUPS_ADD_USER_LOADING = "groups_add_user_loading";
@@ -22,12 +10,3 @@ export const GROUPS_REMOVE_USER_LOADING = "groups_remove_user_loading";
 export const GROUPS_EDIT_USER_SUCCESSFULLY = "groups_edit_user_successfully";
 export const GROUPS_EDIT_USER_FAILED = "groups_edit_user_failed";
 export const GROUPS_EDIT_USER_LOADING = "groups_edit_user_loading";
-
-export const GROUP_GET_LOADING = "group_get_loading";
-export const GROUP_GET_SUCCESSFULLY = "group_get_successfully";
-export const GROUP_GET_FAILED = "group_get_failed";
-
-export const GROUPS_GET_MINIFIED_LOADING = "groups_get_minified_loading";
-export const GROUPS_GET_MINIFIED_SUCCESSFULLY =
-    "groups_get_minified_successfully";
-export const GROUPS_GET_MINIFIED_FAILED = "groups_get_minified_failed";
