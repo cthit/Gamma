299d13f018f0cda2d2d9413f2304be3e1158cb57
Theodor Angergård <portalprogramming@gmail.com>
2020-01-25 14:14:26 +0100

    Remove old ChangeUserPassword route

--- frontend/src/use-cases/users/Users.jsx
+++ frontend/src/use-cases/users/Users.jsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Route, Switch } from "react-router-dom";
-import ChangeUserPassword from "./screens/change-user-password";
 import { DigitCRUD, useDigitTranslations } from "@cthit/react-digit-components";
 import { getUser, getUsersMinified } from "../../api/users/get.users.api";
 import translations from "./Users.translations";
@@ -53,123 +51,89 @@ const Users = () => {
     }
 
     return (
-        <>
-            <Switch>
-                <Route
-                    path="/users/:id/change_password"
-                    exact
-                    component={ChangeUserPassword}
-                />
-                <Route
-                    path={"/users"}
-                    render={() => (
-                        <DigitCRUD
-                            name={"users"}
-                            path={"/users"}
-                            readAllRequest={getUsersMinified}
-                            readOneRequest={admin ? getUser : null}
-                            updateRequest={admin ? editUser : null}
-                            deleteRequest={admin ? deleteUser : null}
-                            createRequest={admin ? addUser : null}
-                            idProp={ID}
-                            keysOrder={[
-                                CID,
-                                PASSWORD,
-                                FIRST_NAME,
-                                LAST_NAME,
-                                NICK,
-                                EMAIL,
-                                ACCEPTANCE_YEAR,
-                                LANGUAGE,
-                                WEBSITES,
-                                USER_AGREEMENT,
-                                GROUPS
-                            ]}
-                            tableProps={{
-                                titleText: text.Users,
-                                startOrderBy: NICK,
-                                search: true,
-                                columnsOrder: [
-                                    CID,
-                                    FIRST_NAME,
-                                    NICK,
-                                    LAST_NAME,
-                                    ACCEPTANCE_YEAR
-                                ]
-                            }}
-                            customDetailsRenders={generateUserCustomDetailsRenders(
-                                text
-                            )}
-                            keysText={generateUserKeysTexts(text)}
-                            formValidationSchema={generateUserValidationSchema(
-                                text,
-                                true,
-                                true,
-                                true
-                            )}
-                            formComponentData={generateUserEditComponentData(
-                                text,
-                                websites
-                            )}
-                            backButtonText={text.Back}
-                            detailsButtonText={text.Details}
-                            createButtonText={text.Create}
-                            updateButtonText={data =>
-                                text.Update + " " + data[NICK]
-                            }
-                            deleteButtonText={data =>
-                                text.Delete + " " + data[NICK]
-                            }
-                            dialogDeleteTitle={() => text.AreYouSure}
-                            dialogDeleteDescription={data =>
-                                text.AreYouSureYouWantToDelete +
-                                " " +
-                                fullName(data) +
-                                "?"
-                            }
-                            detailsTitle={data => fullName(data)}
-                            dialogDeleteConfirm={() => text.Delete}
-                            dialogDeleteCancel={() => text.Cancel}
-                            createTitle={text.CreateUser}
-                            updateTitle={data =>
-                                text.Update + " " + fullName(data)
-                            }
-                            toastUpdateSuccessful={data =>
-                                fullName(data) +
-                                " " +
-                                text.WasUpdatedSuccessfully
-                            }
-                            toastUpdateFailed={data =>
-                                text.UserUpdateFailed1 +
-                                " " +
-                                fullName(data) +
-                                " " +
-                                text.UserUpdateFailed2
-                            }
-                            toastDeleteSuccessful={data =>
-                                fullName(data) +
-                                " " +
-                                text.WasDeletedSuccessfully
-                            }
-                            toastDeleteFailed={data =>
-                                text.UserDeletionFailed1 +
-                                " " +
-                                fullName(data) +
-                                " " +
-                                text.UserDeletionFailed2
-                            }
-                            toastCreateSuccessful={data =>
-                                fullName(data) +
-                                " " +
-                                text.WasCreatedSuccessfully
-                            }
-                            toastCreateFailed={() => text.FailedCreatingUser}
-                            formInitialValues={generateUserInitialValues()}
-                        />
-                    )}
-                />
-            </Switch>
-        </>
+        <DigitCRUD
+            name={"users"}
+            path={"/users"}
+            readAllRequest={getUsersMinified}
+            readOneRequest={admin ? getUser : null}
+            updateRequest={admin ? editUser : null}
+            deleteRequest={admin ? deleteUser : null}
+            createRequest={admin ? addUser : null}
+            idProp={ID}
+            keysOrder={[
+                CID,
+                PASSWORD,
+                FIRST_NAME,
+                LAST_NAME,
+                NICK,
+                EMAIL,
+                ACCEPTANCE_YEAR,
+                LANGUAGE,
+                WEBSITES,
+                USER_AGREEMENT,
+                GROUPS
+            ]}
+            tableProps={{
+                titleText: text.Users,
+                startOrderBy: NICK,
+                search: true,
+                columnsOrder: [
+                    CID,
+                    FIRST_NAME,
+                    NICK,
+                    LAST_NAME,
+                    ACCEPTANCE_YEAR
+                ]
+            }}
+            customDetailsRenders={generateUserCustomDetailsRenders(text)}
+            keysText={generateUserKeysTexts(text)}
+            formValidationSchema={generateUserValidationSchema(
+                text,
+                true,
+                true,
+                true
+            )}
+            formComponentData={generateUserEditComponentData(text, websites)}
+            backButtonText={text.Back}
+            detailsButtonText={text.Details}
+            createButtonText={text.Create}
+            updateButtonText={data => text.Update + " " + data[NICK]}
+            deleteButtonText={data => text.Delete + " " + data[NICK]}
+            dialogDeleteTitle={() => text.AreYouSure}
+            dialogDeleteDescription={data =>
+                text.AreYouSureYouWantToDelete + " " + fullName(data) + "?"
+            }
+            detailsTitle={data => fullName(data)}
+            dialogDeleteConfirm={() => text.Delete}
+            dialogDeleteCancel={() => text.Cancel}
+            createTitle={text.CreateUser}
+            updateTitle={data => text.Update + " " + fullName(data)}
+            toastUpdateSuccessful={data =>
+                fullName(data) + " " + text.WasUpdatedSuccessfully
+            }
+            toastUpdateFailed={data =>
+                text.UserUpdateFailed1 +
+                " " +
+                fullName(data) +
+                " " +
+                text.UserUpdateFailed2
+            }
+            toastDeleteSuccessful={data =>
+                fullName(data) + " " + text.WasDeletedSuccessfully
+            }
+            toastDeleteFailed={data =>
+                text.UserDeletionFailed1 +
+                " " +
+                fullName(data) +
+                " " +
+                text.UserDeletionFailed2
+            }
+            toastCreateSuccessful={data =>
+                fullName(data) + " " + text.WasCreatedSuccessfully
+            }
+            toastCreateFailed={() => text.FailedCreatingUser}
+            formInitialValues={generateUserInitialValues()}
+        />
     );
 };
 
