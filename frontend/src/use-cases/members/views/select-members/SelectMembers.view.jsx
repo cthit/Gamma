9113f8cd6a0dbeea7c35c38e0764bb1baca82f1f
Theodor Angergård <portalprogramming@gmail.com>
2020-02-20 19:18:46 +0100

    Rewrites SelectMembers as a functional component

--- frontend/src/use-cases/members/views/select-members/SelectMembers.view.jsx
+++ frontend/src/use-cases/members/views/select-members/SelectMembers.view.jsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import {
     DigitButton,
     DigitTable,
-    DigitTranslations,
     DigitText,
     DigitLayout,
     DigitDesign,
@@ -19,174 +18,84 @@ import {
     NICK
 } from "../../../../api/users/props.users.api";
 
-const SelectMembers = () => {
-    return null;
+const generateHeaderTexts = text => {
+    const headerTexts = {};
+
+    headerTexts[FIRST_NAME] = text.FirstName;
+    headerTexts[LAST_NAME] = text.LastName;
+    headerTexts[NICK] = text.Nickname;
+    headerTexts[ID] = text.Id;
+    headerTexts["__link"] = text.Link;
+    headerTexts["__checkbox"] = text.Checkbox;
+
+    return headerTexts;
 };
 
-class SelectMembers1 extends React.Component {
-    constructor(props) {
-        super(props);
+const SelectMembers = ({ users, group, groupId, onMembersSelected }) => {
+    const [text] = useDigitTranslations(translations);
+    const [selectedMemberIds, setSelectedMemberIds] = useState(
+        group.groupMembers.map(member => member.id)
+    );
 
-        this.state = {
-            selectedMemberIds: [],
-            currentMembers: [],
-            currentMemberIds: []
-        };
-    }
+    const unsavedEdits = useMemo(() => true, [
+        JSON.stringify(selectedMemberIds)
+    ]);
 
-    componentWillUpdate(nextProps, nextState, nextContext) {
-        if (
-            nextProps.group != null &&
-            nextProps.users != null &&
-            !nextState.hasLoadedCurrentMembers
-        ) {
-            const group = nextProps.group;
-
-            var selectedMembers = sessionStorage.getItem(
-                group.id + ".selectedMembers"
-            );
-
-            if (selectedMembers != null) {
-                selectedMembers = JSON.parse(selectedMembers);
-            }
-
-            if (selectedMembers == null || selectedMembers.length === 0) {
-                selectedMembers = group.groupMembers.map(member => member.id);
-            }
-
-            this.setState(
-                {
-                    currentMembers: group.groupMembers,
-                    currentMemberIds: group.groupMembers.map(
-                        member => member.id
-                    ),
-                    selectedMemberIds: selectedMembers,
-                    hasLoadedCurrentMembers: true
-                },
-                () => {
-                    this._save(selectedMembers);
-                }
-            );
-        }
-    }
-
-    _save = selectedMembers => {
-        sessionStorage.setItem(
-            this.props.group.id + ".selectedMembers",
-            JSON.stringify(selectedMembers)
-        );
-    };
-
-    _unsavedEdits = () => {
-        const { selectedMemberIds, currentMemberIds } = this.state;
-
-        return !_.isEqual(
-            _.sortBy(selectedMemberIds),
-            _.sortBy(currentMemberIds)
-        );
-    };
-
-    componentWillUnmount() {
-        const { selectedMemberIds } = this.state;
-
-        this._save(selectedMemberIds);
-    }
-
-    onSelectedChange = selected => {
-        this.setState({
-            selectedMemberIds: selected,
-            unsavedEdits: selected.length > 0
-        });
-
-        this._save(selected);
-    };
-
-    generateHeaderTexts = text => {
-        const headerTexts = {};
-
-        headerTexts[FIRST_NAME] = text.FirstName;
-        headerTexts[LAST_NAME] = text.LastName;
-        headerTexts[NICK] = text.Nickname;
-        headerTexts[ID] = text.Id;
-        headerTexts["__link"] = text.Link;
-        headerTexts["__checkbox"] = text.Checkbox;
-
-        return headerTexts;
-    };
-
-    render() {
-        const [text] = useDigitTranslations(translations);
-
-        const { selectedMemberIds, currentMembers } = this.state;
-        const { users, group, onMembersSelected } = this.props;
-
-        const unsavedEdits = this._unsavedEdits();
-
-        if (
-            group != null &&
-            users != null &&
-            (currentMembers.length > 0 ? users.length > 0 : true)
-        ) {
-            return (
-                <DigitLayout.Column>
-                    <DigitDesign.Card>
-                        <DigitDesign.CardBody>
-                            <DigitLayout.Row justifyContent={"space-between"}>
-                                <DigitText.Heading5
-                                    text={
-                                        unsavedEdits
-                                            ? text.UnsavedEdits
-                                            : text.NoChanges
-                                    }
-                                />
-                                <div>
-                                    <DigitButton
-                                        disabled={!unsavedEdits}
-                                        raised
-                                        primary
-                                        text={"Nästa"}
-                                        onClick={() => {
-                                            onMembersSelected(
-                                                selectedMemberIds
-                                            );
-                                        }}
-                                    />
-                                </div>
-                            </DigitLayout.Row>
-                        </DigitDesign.CardBody>
-                    </DigitDesign.Card>
-                    <DigitLayout.Row>
-                        <UsersInGroupChanges
-                            currentMembers={currentMembers}
-                            selectedMembers={selectedMemberIds.map(memberId =>
-                                _.find(users, { id: memberId })
-                            )}
+    return (
+        <DigitLayout.Column>
+            <DigitDesign.Card>
+                <DigitDesign.CardBody>
+                    <DigitLayout.Row justifyContent={"space-between"}>
+                        <DigitText.Heading5
+                            text={
+                                unsavedEdits
+                                    ? text.UnsavedEdits
+                                    : text.NoChanges
+                            }
                         />
-                        <DigitLayout.Column>
-                            <DigitTable
-                                selected={selectedMemberIds}
-                                onSelectedUpdated={this.onSelectedChange}
-                                search
-                                titleText={text.UsersFor + group[PRETTY_NAME]}
-                                searchText="Search for users"
-                                showSearchableProps
-                                idProp="id"
-                                startOrderBy={NICK}
-                                columnsOrder={[FIRST_NAME, NICK, LAST_NAME]}
-                                headerTexts={this.generateHeaderTexts(text)}
-                                data={users.map(user => ({
-                                    ...user,
-                                    __link: "/users/" + user.cid
-                                }))}
+                        <div>
+                            <DigitButton
+                                disabled={!unsavedEdits}
+                                raised
+                                primary
+                                text={"Nästa"}
+                                onClick={() => {
+                                    onMembersSelected(selectedMemberIds);
+                                }}
                             />
-                        </DigitLayout.Column>
+                        </div>
                     </DigitLayout.Row>
+                </DigitDesign.CardBody>
+            </DigitDesign.Card>
+            <DigitLayout.Row>
+                <UsersInGroupChanges
+                    currentMembers={group.groupMembers}
+                    selectedMembers={selectedMemberIds.map(memberId =>
+                        _.find(users, { id: memberId })
+                    )}
+                />
+                <DigitLayout.Column>
+                    <DigitTable
+                        selected={selectedMemberIds}
+                        onSelectedUpdated={newSelected => {
+                            setSelectedMemberIds(newSelected);
+                        }}
+                        search
+                        titleText={text.UsersFor + group[PRETTY_NAME]}
+                        searchText="Search for users"
+                        idProp="id"
+                        startOrderBy={NICK}
+                        columnsOrder={[FIRST_NAME, NICK, LAST_NAME]}
+                        headerTexts={generateHeaderTexts(text)}
+                        data={users.map(user => ({
+                            ...user,
+                            __link: "/users/" + user.cid
+                        }))}
+                    />
                 </DigitLayout.Column>
-            );
-        } else {
-            return null;
-        }
-    }
-}
+            </DigitLayout.Row>
+        </DigitLayout.Column>
+    );
+};
 
 export default SelectMembers;
