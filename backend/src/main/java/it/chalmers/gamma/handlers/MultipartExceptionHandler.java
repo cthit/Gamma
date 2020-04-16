28ba024a936d9fed6991974e412761439277202c
gurr1 <gustav.engsmyre@gmail.com>
2020-04-16 15:56:48 +0200

    add handling for too large files

---
+++ backend/src/main/java/it/chalmers/gamma/handlers/MultipartExceptionHandler.java
@@ -0,0 +1,24 @@
+package it.chalmers.gamma.handlers;
+
+import it.chalmers.gamma.response.FileTooLargeResponse;
+import org.apache.tomcat.util.http.fileupload.FileUploadBase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+@ControllerAdvice
+@RestController
+public class MultipartExceptionHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MultipartExceptionHandler.class);
+
+
+    @ExceptionHandler({FileUploadBase.FileSizeLimitExceededException.class, MaxUploadSizeExceededException.class})
+    public FileTooLargeResponse handleUploadSizeException() {
+        LOGGER.info("Too large file upload was attempted");
+        return new FileTooLargeResponse();
+    }
+}
