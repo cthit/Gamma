4d1548b1a0e18b5115c9589117bf16f29d0d95d0
gurr1 <gustav.engsmyre@gmail.com>
2020-04-20 13:30:20 +0200

    add a default avatar image

---
+++ backend/src/main/java/it/chalmers/gamma/bootstrap/MiscBootstrap.java
@@ -0,0 +1,45 @@
+package it.chalmers.gamma.bootstrap;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MiscBootstrap {
+
+    @Value("classpath:image/default.jpg")
+    private Resource defaultResourceFile;
+
+    @Value("${application.files.path}")
+    private String targetDir;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MiscBootstrap.class);
+
+
+    public void runImageBootstrap() {
+        File targetFile = new File(String.format("%s/%s", targetDir, "default.jpg"));
+        if(!targetFile.exists()) {
+            LOGGER.info("Default Avatar file does not exist, creating a new one");
+                try {
+                    File defaultFile = this.defaultResourceFile.getFile();
+                    if (defaultFile.isFile()) {
+                        File targetDirFile = new File(targetDir);
+                        if (!targetDirFile.mkdir()) {
+                            LOGGER.warn("Could not create target directory");
+                        }
+                        Files.copy(defaultFile.toPath(), targetFile.toPath());
+                    }
+                    else {
+                        throw new IOException();
+                    }
+                } catch (IOException e) {
+                    LOGGER.warn("Could not add a default avatar image");
+                }
+        }
+    }
+}
