e0c9dbe40fd1fbacd3f0acb7869c0a947c2ecf7a
gurr1 <gustav.engsmyre@gmail.com>
2020-05-04 21:50:28 +0200

    Add generic logging for all exceptions with no information logging

--- backend/src/main/java/it/chalmers/gamma/response/client/ITClientDoesNotExistException.java
+++ backend/src/main/java/it/chalmers/gamma/response/client/ITClientDoesNotExistException.java
@@ -1,9 +1,9 @@
 package it.chalmers.gamma.response.client;
 
+import it.chalmers.gamma.response.CustomResponseStatusException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 
-public class ITClientDoesNotExistException extends ResponseStatusException {
+public class ITClientDoesNotExistException extends CustomResponseStatusException {
     public ITClientDoesNotExistException() {
         super(HttpStatus.UNPROCESSABLE_ENTITY, "NO_SUCH_CLIENT_EXISTS");
     }
