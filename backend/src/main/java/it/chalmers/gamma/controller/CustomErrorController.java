8ce8456005f2542057209a1bee50c901bfa07d41
Gustav Engsmyre <gustav.engsmyre@gmail.com>
2020-06-03 21:10:04 +0200

    Improve 404 page

--- backend/src/main/java/it/chalmers/gamma/controller/CustomErrorController.java
+++ backend/src/main/java/it/chalmers/gamma/controller/CustomErrorController.java
@@ -3,6 +3,7 @@ package it.chalmers.gamma.controller;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.RequestDispatcher;
@@ -12,11 +13,13 @@ import javax.servlet.http.HttpServletRequest;
 public class CustomErrorController implements ErrorController {
 
     @GetMapping("/error")
-    public String displayError(HttpServletRequest request) {
+    public String displayError(HttpServletRequest request, Model model) {
         Object statusCodeString = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         int statusCode = Integer.parseInt(statusCodeString.toString());
         if (HttpStatus.NOT_FOUND.value() == statusCode) {
-            return "error-404";
+            model.addAttribute("original_page",
+                    request.getAttribute(RequestDispatcher.FORWARD_REQUEST_URI));
+            return "error-5xx";
         }
         if(HttpStatus.valueOf(statusCode).is5xxServerError()) {
             return "error-5xx";
