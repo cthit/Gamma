19d228df95cd31b0ca2d6abf54250f42df0ef69c
Theodor Angergård <portalprogramming@gmail.com>
2020-06-20 00:15:27 +0200

    Remove unused console.log

--- frontend/src/common/utils/configs/envVariablesLoader.js
+++ frontend/src/common/utils/configs/envVariablesLoader.js
@@ -1,6 +1,5 @@
 function getBackendUrl() {
     var backendUrl = process.env.REACT_APP_BACKEND_URL;
-    console.log(process.env);
     return backendUrl == null ? "http://localhost:8081/api" : backendUrl;
 }
 
