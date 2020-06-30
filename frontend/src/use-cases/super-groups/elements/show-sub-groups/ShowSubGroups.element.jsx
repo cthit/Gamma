e3ed61701431f3a3829da81255b1ff5b27a0013b
Theodor Angergård <portalprogramming@gmail.com>
2020-06-30 15:38:02 +0200

    Rename ShowSuperGroups to ShowSubGroups

--- frontend/src/use-cases/super-groups/elements/show-sub-groups/ShowSubGroups.element.jsx
+++ frontend/src/use-cases/super-groups/elements/show-sub-groups/ShowSubGroups.element.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { DigitText, useDigitTranslations } from "@cthit/react-digit-components";
 
-import translations from "./ShowSuperGroups.element.translations";
+import translations from "./ShowSubGroups.element.translations";
 import DisplayGroupsTable from "../../../../common/elements/display-groups-table/DisplayGroupsTable.element";
 
 const ShowSubGroups = ({ title, subGroups, margin }) => {
