7a5ecb819f169ed787e2c16fadb31404577f163e
Gustav Engsmyre <gustav.engsmyre@gmail.com>
2020-10-31 17:24:29 +0100

    change how redis connections are done to be more explicit

---
+++ backend/src/main/java/it/chalmers/gamma/config/SessionsConfig.java
@@ -0,0 +1,34 @@
+package it.chalmers.gamma.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession
+public class SessionsConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${redis.host}")
+    private String redisHost;
+
+    @Value("${redis.password}")
+    private String redisPassword;
+
+    @Value("${redis.port}")
+    private int redisPort;
+
+    @Bean
+    public LettuceConnectionFactory connectionFactory() {
+        RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration();
+        redisConfiguration.setHostName(redisHost);
+        redisConfiguration.setPassword(redisPassword);
+        redisConfiguration.setPort(redisPort);
+        return new LettuceConnectionFactory(redisConfiguration);
+    }
+}
