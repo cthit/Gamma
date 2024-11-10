3fb580b3eb524997aacfb3ae1173b74eae2ca16a
Oscar Eriksson <oscariremma@gmail.com>
2024-11-10 18:55:53 +0100

    Fix short session times (#921)

--- app/src/main/java/it/chalmers/gamma/security/SessionConfig.java
+++ app/src/main/java/it/chalmers/gamma/security/SessionConfig.java
@@ -5,10 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
-@EnableRedisHttpSession
 public class SessionConfig {
 
   @Value("${spring.data.redis.host}")
